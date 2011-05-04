08eb6a12e4c8e8adc73ce0427544c089e546be98
Kris Zyp <kriszyp@gmail.com>
2011-05-04 11:30:01 -0600

    Upgrade Tree to latest

--- Tree.js
+++ Tree.js
@@ -10,50 +10,50 @@ return declare([], {
 		// summary:
 		//		Renders a cell that can be expanded, creating more rows
 		var level = options.query.level + 1;
-		level = isNaN(level) ? 0 : level;
-		var expanded, table = this.table;
+		var table = this.table;
 		// create the expando
 		var expando = td.appendChild(document.createElement("div"));
+		expando.level = isNaN(level) ? 0 : level;
 		if(this.field){
 			td.appendChild(document.createTextNode(data));
 		}
-		expando.className = "ui-icon ui-icon-triangle-1-e";
-		expando.setAttribute("style", "margin-left: " + (level * 19) + "px; float: left");
+		expando.className = "dojoxGridxExpando ui-icon ui-icon-triangle-1-e";
+		expando.setAttribute("style", "margin-left: " + (expando.level * 19) + "px; float: left");
 		var tr, query;
-		var preloadNode;
-		expando.onclick = function(){
-			// on click we toggle expanding and collapsing
-			if(!preloadNode){
-				// if the children have not been created, create a preload node and do the 
-				// query for the children
-				tr = td.parentNode;
-				preloadNode = document.createElement("tr");
-				query.level = level;
-				tr.parentNode.insertBefore(preloadNode, tr.nextSibling);
-				var object = table.getObject(tr);
-				table.renderQuery(query, preloadNode);
-			}
-			function query(options){
-				return table.store.getChildren(object, options);
-			}
-			// update the expando display
-			var styleDisplay = (expanded = !expanded) ? "" : "none";
-			if(expanded){
-				expando.className = "ui-icon ui-icon-triangle-1-se";
-			}else{
-				expando.className = "ui-icon ui-icon-triangle-1-e";
-			}
-			// show or hide all the children
-			var childTr = tr;
-			do{
-				childTr = childTr.nextSibling;
-				if(childTr){
-					childTr.style.display = styleDisplay;
-				} 
-			}while(childTr && (childTr != preloadNode));
+
+		if(!table._hasTreeListener){
+			// just setup the event listener once and use event delegation for better memory use
+			table._hasTreeListener = true;
+			this.table.on(".dojoxGridxExpando:click", function(event){
+				// on click we toggle expanding and collapsing
+				var expanded = this.expanded = !this.expanded;
+				// update the expando display
+				this.className = "dojoxGridxExpando ui-icon ui-icon-triangle-1-" + (expanded ? "se" : "e"); 
+				var preloadNode = this.preloadNode;
+				var row = table.getRowNode(table.getObjectId(this));
+				if(!preloadNode){
+					// if the children have not been created, create a preload node and do the 
+					// query for the children
+					preloadNode = this.preloadNode = document.createElement("div");
+					var query = function(options){
+						return table.store.getChildren(object, options);
+					};
+					query.level = this.level;
+					row.parentNode.insertBefore(preloadNode, row.nextSibling);
+					var object = table.getObject(row);
+					table.renderQuery(query, preloadNode);
+				}
+				// show or hide all the children
+				var styleDisplay = expanded ? "" : "none";
+				var childTr = row;
+				do{
+					childTr = childTr.nextSibling;
+					if(childTr){
+						childTr.style.display = styleDisplay;
+					} 
+				}while(childTr && (childTr != preloadNode));
+			});
 		};
-		
-		
 	}
 });
 });
