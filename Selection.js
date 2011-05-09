2481f860055d5502796080cc64865fdfe4e225a1
Kris Zyp <kriszyp@gmail.com>
2011-05-09 13:16:37 -0600

    Making plugins be mixins

--- Selection.js
+++ Selection.js
@@ -1,44 +1,78 @@
-define(["dojo/Stateful", "dojo/listen"], function(Stateful, listen){
-return function(table, options){
+define(["dojo/_base/declare", "dojo/Stateful", "dojo/listen"], function(declare, Stateful, listen){
+return declare([], {
 	// summary:
-	// 		Add selection capabilities to a table
-	// returns:
-	// 		A stateful object (get/set/watch) where the property names correspond to 
-	// 		object ids and values are true or false depending on whether an item is selected 
-	var mode = (options && options.mode) || "extended";
-	listen(table.contentNode, ".dojoxGridxRow:click, .dojoxGridxRow:keydown", function(e){
-		if(e.type == "click" || e.keyCode == 32){
-			if(e.keyCode == 32){
-				e.preventDefault();
-			}
-			var id = table.getObjectId(e);
-			switch(mode){
-				case "single":
-					for(var i in selection){
-						if(selection.hasOwnProperty(i)){
-							set(this, i, false);
+	// 		Add selection capabilities to a table. The grid will have a selection property and
+	//		fire "select" and "deselect" events.
+	postCreate: function(){
+		this.inherited(arguments);
+		var lastRow, mode = (this.selectionMode) || "multiple";
+		var table = this;
+		this.selection = new Stateful();
+		if(this.selectionMode != "none"){
+			listen(this.contentNode, ".dojoxGridxRow:click, .dojoxGridxRow:keydown", function(e){
+				if(e.type == "click" || e.keyCode == 32){
+					e.preventDefault();
+					var thisRow = table.row(e);
+					var targetElement = thisRow.element;
+					var selection = table.selection;
+					var id = thisRow.id;
+					if(mode == "single" || (!event.ctrlKey && mode == "multiple")){
+						
+						for(var i in selection){
+							if(selection.hasOwnProperty(i) && typeof selection[i] != "function"){
+								set(table, targetElement, i, false);
+							}
 						}
-					}					
-					set(this, id, true);
-					break;
-				case "extended":
-					set(this, id, !selection[id]);
-				 	break;
-			}
+						set(table, targetElement, id, true);
+					}else{
+						set(table, targetElement, id, !selection[id]);
+					}
+					if(event.shiftKey){
+						var lastElement = lastRow && lastRow.element;
+						// find if it is earlier or later in the DOM
+						var traverser = (lastElement && (lastElement.compareDocumentPosition ? 
+							lastElement.compareDocumentPosition(targetElement) == 2 :
+							lastElement.sourceIndex > targetElement.sourceIndex)) ? "nextSibling" : "previousSibling";
+						var nextNode;
+						while(nextNode = thisRow.element[traverser]){
+							// loop through and set everything
+							thisRow = table.row(nextNode);
+							set(table, thisRow.element, thisRow.id, true);
+							if(nextNode == lastElement){
+								break;
+							}
+						}
+					}else{
+						lastRow = thisRow;
+					}
+				}
+				
+			});
 		}
-	});
-	var selection = new Stateful();
-	function set(target, id, value){
-		if(listen.dispatch(target, value ? "select" : "deselect", {
-			cancelable: true,
-			bubbles: true
-		})){
-			selection.set(id, value);
-		}
+		table.selection.watch(function(id, oldValue, value){
+			dojo[value ? "addClass" : "removeClass"](table.row(id).element, "dojoxGridxRowSelected");
+			dojo[value ? "addClass" : "removeClass"](table.row(id).element, "ui-state-active");
+		});
+	},
+	// selection:
+	// 		A stateful object (get/set/watch) where the property names correspond to 
+	// 		object ids and values are true or false depending on whether an item is selected
+	selection: {},
+	select: function(id){
+		set(this, this.row(id).element, id, true);
+	},
+	deselect: function(id){
+		set(this, this.row(id).element, id, false);
 	}
-	selection.watch(function(id, oldValue, value){
-		dojo[value ? "addClass" : "removeClass"](table.getRowNode(id), "dojoxGridxRowSelected");
-	});
-	return selection;
-};
+});
+function set(table, target, id, value){
+	if(listen.dispatch(target, value ? "select" : "deselect", {
+		cancelable: true,
+		bubbles: true,
+		id: id
+	})){
+		table.selection.set(id, value);
+	}
+}
+
 });
