9857019cedafebc5e6bf6a8112b7fe87b8e2899e
Kris Zyp <kriszyp@gmail.com>
2011-05-18 16:28:02 -0600

    Fix id

--- tests/test_data.js
+++ tests/test_data.js
@@ -27,11 +27,14 @@ define(["dojo/store/Memory", "dojo/store/Observable"],function(Memory, Observabl
 	var typesData = [];
 	for(var i = 0; i < 20; i++){
 		typesData.push({
+			id: i,
 			integer: Math.floor(Math.random() * 100),
 			floatNum: Math.random() * 100,
 			date: new Date(new Date().getTime() * Math.random() * 2),
 			date2: new Date(new Date().getTime() - Math.random() * 1000000000),
-			text: "A number in text " + Math.random()
+			text: "A number in text " + Math.random(),
+			bool: Math.random() > 0.5,
+			bool2: Math.random() > 0.5
 		});
 	}
 	testTypesStore = Observable(Memory({data: typesData}));
