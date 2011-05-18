d79d5a07231f007a73b9caf6232ba2d2378b16e5
Kris Zyp <kriszyp@gmail.com>
2011-05-18 16:27:56 -0600

    Added generic Editor column extender

---
+++ Editor.js
@@ -0,0 +1,88 @@
+define(["dojo/listen"], function(listen){
+
+return function(column, editor, editOn){
+	// summary:
+	// 		Add a column with text editing capability
+	var originalRenderCell = column.renderCell || function(data, td){
+		if(data != null){
+			td.appendChild(document.createTextNode(data));
+		}
+	};
+	column.editor = editor = editor || column.editor;
+	column.editOn = editOn = editOn || column.editOn;
+	var grid;
+	var renderWidget = typeof editor == "string" ?
+		function(data, cell, object, onblur){
+			// it is string editor, so we use a common <input> as the editor
+			var input = dojo.create("input",{
+				type: editor,
+				className: "dojoxGridxInput",
+				value: data,
+				checked: data
+			}, cell);
+			var id = grid.row(object).id;
+			if(onblur){
+				input.focus();
+				input.onblur = function(){
+					// delete the input now
+					var thisInput = input;
+					input = null;
+					cell.removeChild(thisInput);
+					onblur(data);
+				};
+			}
+			input.onchange = function(){
+				if(input){
+					data = setProperty(cell, data, input[editor == "checkbox" ? "checked" : "value"]);
+				}
+			}
+		} :
+		function(data, cell, object, onblur){
+			// using a widget as the editor
+			var widget = new editor(column, cell.appendChild(document.createElement("div")));
+			widget.set("value", data);
+			widget.watch("value", function(key, oldValue, value){
+				data = setProperty(cell, data, value);
+			});
+			if(onblur){
+				widget.focus();
+				widget.connect(widget, "onBlur", function(){
+					widget.destroy();
+					onblur(data);
+				});
+			}
+		};
+	function setProperty(cell, oldValue, value){
+		if(listen.emit(cell, "change", {oldValue: oldValue, value: value, bubbles: true, cancelable: true})){
+			var row = grid.row(cell);
+			var object = row.data;
+			var dirty = grid.dirty[row.id] || (grid.dirty[row.id] = {});
+			dirty[column.field] = object[column.field] = value;
+			if(column.autoSave){
+				grid.save();
+			}
+			return value;
+		}
+		// else keep the value the same
+		return oldValue;
+	}
+	column.renderCell = function(data, cell, options, object){
+		grid = column.grid;
+		if(column.editOn){
+			listen(cell, column.editOn, function(){
+				cell.innerHTML = "";
+				renderWidget(data, cell, object, function(newData){
+					originalRenderCell(data = newData, cell);
+				});
+			});
+			originalRenderCell(data, cell);
+		}else{
+			renderWidget(data, cell, object);
+		}
+	}
+	
+	return column;
+};
+});
+
+
