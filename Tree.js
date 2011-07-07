ba8d1ba05e2bd3a4cbaffafd1bc89e2cd88ff0db
Kris Zyp <kriszyp@gmail.com>
2011-07-07 16:04:34 -0600

    Support alternate event types to trigger expansion

--- Tree.js
+++ Tree.js
@@ -1,4 +1,4 @@
-define(["dojo/_base/declare", "xstyle/create", "dojo/_base/Deferred"], function(declare, create, Deferred){
+define(["dojo/_base/declare", "xstyle/create", "dojo/_base/Deferred", "dojo/query"], function(declare, create, Deferred, querySelector){// TODO: eliminate the dojo/query dep by supporting simple event delegation in the List module
 
 return function(column){
     // summary:
@@ -14,51 +14,50 @@ return function(column){
 		var level = options.query.level + 1;
 		level = isNaN(level) ? 0 : level;
 		var grid = this.grid;
+		var mayHaveChildren = !grid.store.mayHaveChildren || grid.store.mayHaveChildren(object);
 		// create the expando
-		var expando = create(td, ".dgrid-expando-icon" + (!grid.store.mayHaveChildren || 
-			grid.store.mayHaveChildren(object) ? ".ui-icon.ui-icon-triangle-1-e" : "") +
+		var expando = create(td, ".dgrid-expando-icon" + (mayHaveChildren ? ".ui-icon.ui-icon-triangle-1-e" : "") +
 			"[style=margin-left: " + (level * 19) + "px; float: left]");
 		originalRenderCell.call(this, object, value, td, options);
 		expando.level = level;
+		expando.mayHaveChildren = mayHaveChildren;
 		var tr, query;
 
 		if(!grid._hasTreeListener){
 			// just setup the event listener once and use event delegation for better memory use
 			grid._hasTreeListener = true;
-			this.grid.on("click", function(event){
-				var target = event.target;
-				while(target.className.indexOf("dgrid-expando-icon") < 0){
-					if("className" in (target = target.parentNode)){
-						return;
+			this.grid.on(column.expandOn || ".dgrid-content .column-" + column.id + ":click", function(event){
+				var target = this.className.indexOf("dgrid-expando-icon") > -1 ? this :
+					querySelector(".dgrid-expando-icon", this)[0];
+				if(target.mayHaveChildren){
+					// on click we toggle expanding and collapsing
+					var expanded = target.expanded = !target.expanded;
+					// update the expando display
+					target.className = "dgrid-expando-icon ui-icon ui-icon-triangle-1-" + (expanded ? "se" : "e"); 
+					var preloadNode = target.preloadNode;
+					var row = grid.row(target);
+					var rowElement = row.element;
+					if(!preloadNode){
+						// if the children have not been created, create a container, a preload node and do the 
+						// query for the children
+						var container = rowElement.connected = create('.dgrid-tree-container');
+						preloadNode = target.preloadNode = create(container, 'div');
+						//preloadNode.nextRow = grid.down(row).element;
+						var query = function(options){
+							return grid.store.getChildren(row.data, options);
+						};
+						query.level = target.level;
+						rowElement.parentNode.insertBefore(container, rowElement.nextSibling);
+						grid.renderQuery ? 
+							grid.renderQuery(query, preloadNode) :
+							grid.renderArray(query({}), preloadNode);
 					}
+					// show or hide all the children
+					var styleDisplay = expanded ? "" : "none";
+					container = rowElement.connected;
+					container.rowContainer = expanded;
+					container.style.display = styleDisplay;// TODO: maybe use a CSS class here
 				}
-				// on click we toggle expanding and collapsing
-				var expanded = target.expanded = !target.expanded;
-				// update the expando display
-				target.className = "dgrid-expando-icon ui-icon ui-icon-triangle-1-" + (expanded ? "se" : "e"); 
-				var preloadNode = target.preloadNode;
-				var row = grid.row(target);
-				var rowElement = row.element;
-				if(!preloadNode){
-					// if the children have not been created, create a container, a preload node and do the 
-					// query for the children
-					var container = rowElement.connected = create('.dgrid-tree-container');
-					preloadNode = target.preloadNode = create(container, 'div');
-					//preloadNode.nextRow = grid.down(row).element;
-					var query = function(options){
-						return grid.store.getChildren(row.data, options);
-					};
-					query.level = target.level;
-					rowElement.parentNode.insertBefore(container, rowElement.nextSibling);
-					grid.renderQuery ? 
-						grid.renderQuery(query, preloadNode) :
-						grid.renderArray(query({}), preloadNode);
-				}
-				// show or hide all the children
-				var styleDisplay = expanded ? "" : "none";
-				container = rowElement.connected;
-				container.rowContainer = expanded;
-				container.style.display = styleDisplay;// TODO: maybe use a CSS class here
 			});
 		};
 	};
