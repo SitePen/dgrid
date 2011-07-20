2d10a31e9d8a6ade67b4e3c4786d9d389c1f5c3e
Kris Zyp <kriszyp@gmail.com>
2011-07-20 11:33:35 -0600

    Fix outline for safari too

--- ColumnSet.js
+++ ColumnSet.js
@@ -8,9 +8,9 @@ function(styleSheet, has, put, declare, listen, aspect, query, Grid){
 		// need position: relative in old IE to work properly
 		styleSheet.addRule("table.dgrid-row", "position: relative");
 	}
-	if(has("mozilla")){
+	if(has("mozilla") || has("safari")){
 		// firefox and opera's outline gets cropped by the overflow: hidden 
-		styleSheet.addRule(".dgrid *:focus", "border-style: dotted;border-color: black");
+		styleSheet.addRule(".dgrid-column-set *:focus", "border: 1px dotted black; outline: 1px dotted black");
 	}
 	return declare([Grid], {
 		columnSets: [],
