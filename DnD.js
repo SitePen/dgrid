1a7f0667519d30f548d541a4b840fcfc9b7ecc3c
Kris Zyp <kriszyp@gmail.com>
2011-08-08 22:27:04 -0600

    Much more progress on DnD

--- DnD.js
+++ DnD.js
@@ -1,30 +1,56 @@
-define(["dojo/_base/declare", "./List", "dojo/dnd/Source", "xstyle/put", "dojo/on"], function(declare, List, DnDSource, put, on){
-	return declare([List], {
-		type: "row",
-		postCreate: function(){
-			this.inherited(arguments);
-			var grid = this;
-			var store = this.store;
-			// make the contents a DnD source/target
-			var source = new DnDSource(this.contentNode, {accept:[this.type]});
-			source.onDrop= function(source, nodes){
-				// on drop, move get the objects and move them
-				var targetRow = grid.row(source.targetAnchor);
-				dojo.when(store.get(targetRow.id), function(target){
-					nodes.forEach(function(node){
-						var row = grid.row(node);
-						dojo.when(store.get(row.id), function(object){
-							store.put(object, { before: target});
-						});
+define(["dojo/_base/declare", "dojo/_base/lang", "./List", "dojo/dnd/Source", "xstyle/put", "dojo/on", "xstyle/css!dojo/resources/dnd.css"], function(declare, lang, List, DnDSource, put, on){
+/*	var GridDnDSource = new declare(DnDSource, {
+		getAllNodes: function(){
+			return dojo.query("> .dojoDndItem", this.parent);	// NodeList
+		}	
+	});*/
+	function setupDnD(grid){
+		if(grid.dndTarget){
+			return;
+		}
+		var store = grid.store;
+		// make the contents a DnD source/target
+		var targetSource = grid.dndTarget = new DnDSource(grid.bodyNode, 
+			lang.delegate(grid.dndTargetConfig, {dropParent: grid.contentNode}));
+		targetSource.getObject = function(node){
+			var row = grid.row(node);
+			return store.get(row.id);
+		}
+		targetSource.onDrop = function(sourceSource, nodes){
+			// on drop, move get the objects and move them
+			var targetRow = targetSource.targetAnchor && grid.row(targetSource.targetAnchor);
+			dojo.when(targetRow && store.get(targetRow.id), function(target){
+				nodes.forEach(function(node){
+					dojo.when(sourceSource.getObject(node), function(object){
+						store.put(object, {before: target});
 					});
 				});
-				//return true;
-			};
+			});
+		};
+	}
+	return declare([List], {
+		dndSourceType: "row",
+		dndTargetConfig: {
+			accept: ["row"],
+			creator: function(){
+				return {
+					node: put("div"),
+					data: {},
+					type: []
+				};
+			}
+		}, 
+		postCreate: function(){
+			this.inherited(arguments);
+			setupDnD(this);
 		},
-		renderRow: function(){
+		insertRow: function(object){
 			// override to add dojoDndItem class to make the rows draggable
 			var row = this.inherited(arguments);
 			put(row, '.dojoDndItem');
+			// setup the source if it hasn't been done yet
+			setupDnD(this);
+			this.dndTarget.setItem(row.id, {data: object, type: [this.dndSourceType]});
 			return row;
 		}
 	});
