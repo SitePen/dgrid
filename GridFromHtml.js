3e246b98ab05d9e0f8b1f8b834729174d606154e
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2011-08-12 18:36:36 -0400

    Add support for specifying ColumnSets in HTML
    * Added GridWithColumnSetsFromHtml module for cases where ColumnSet
      support via HTML colgroups is of interest
    * Largely refactored GridFromHtml to make parts reusable by
      GridWithColumnSetsFromHtml; the former still works as it did before
    * Added test to test_complex_column
    
    GridWithColumnSetsFromHtml is subject to renaming if someone can
    think of something shorter that's still descriptive enough.
    ("Fred" doesn't count.)

--- GridFromHtml.js
+++ GridFromHtml.js
@@ -1,64 +1,87 @@
-define(["./Grid", "dojo/_base/declare", "dojo/_base/lang"], function(Grid, declare, lang){
-	// TODO: Maybe support getting the data from the <tbody> as well
+define(["./Grid", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Deferred", "require"], function(Grid, declare, lang, Deferred, require){
+	// This module supports parsing grid structure information from an HTML table.
+	// This module does NOT support ColumnSets; see GridWithColumnSetsFromHtml
 	
-	// Functions for getting various types of values from HTML attributes
-	function getObjFromAttr(node, attr){
-		// used to for e.g. formatter, get, renderCell
-		var val = node.getAttribute(attr);
-		console.log('getObjFromAttr:', node, attr, ' => ', val);
-		return val && lang.getObject(val);
-	}
-	function getBoolFromAttr(node, attr){
-		// used for e.g. sortable
-		var val = node.getAttribute(attr);
-		return val && val !== "false";
-	}
-	function getNumFromAttr(node, attr){
-		// used for e.g. rowSpan, colSpan
-		var val = node.getAttribute(attr);
-		val = val && Number(val);
-		return isNaN(val) ? undefined : val;
-	}
-	
-	function createColumnsFromDom(domNode, columns){
+	function getSubRowsFromDom(domNode){
 		// summary:
 		//		generate columns from DOM. Should this be in here, or a separate module?
-		var trs = domNode.getElementsByTagName("tr"), trslen = trs.length;
+		var
+			columns = [], // to be pushed upon / returned
+			trs = domNode.getElementsByTagName("tr"),
+			trslen = trs.length,
+			getCol = GridFromHtml.utils.getColumnFromCell;
+		
 		for(var i = 0; i < trslen; i++){
 			var rowColumns = [];
 			columns.push(rowColumns);
 			var tr = trs[i];
 			var ths = tr.getElementsByTagName("th"), thslen = ths.length;
 			for(var j = 0; j < thslen; j++){
-				var th = ths[j];
-				rowColumns.push({
-					name: th.innerHTML,
-					field: th.getAttribute("field") || th.className || th.innerHTML,
-					className: th.className,
-					sortable: getBoolFromAttr(th, "sortable"),
-					get: getObjFromAttr(th, "get"),
-					formatter: getObjFromAttr(th, "formatter"),
-					renderCell: getObjFromAttr(th, "renderCell"),
-					rowSpan: getNumFromAttr(th, "rowspan"),
-					colSpan: getNumFromAttr(th, "colspan")
-				});
+				rowColumns.push(getCol(ths[j]));
 			}
 		}
 		if(tr){
+			// FIXME: this assumes that applicable TRs were ONLY found under one
+			// grouping element!  Maybe should limit to thead like dojox grid?
+			// (Especially if we ever want to support tbody>tr>td -> renderArray)
 			domNode.removeChild(tr.parentNode);
 		}
+		
+		return columns;
 	}
-	return declare([Grid], {
+	
+	var GridFromHtml = declare([Grid], {
 		configStructure: function(){
 			// summary:
 			//		Configure subRows based on HTML originally in srcNodeRef
-			if(!this.checkedTrs){
-				this.checkedTrs = true;
-				// set this.subRows, then let createColumnsFromDom push to it
-				this.subRows = [];
-				createColumnsFromDom(this.domNode, this.subRows);
+			if(!this._checkedTrs){
+				this._checkedTrs = true;
+				this.subRows = getSubRowsFromDom(this.domNode, this.subRows);
 			}
 			return this.inherited(arguments);
 		}
 	});
+	
+	// hang some utility functions, potentially useful for extensions
+	GridFromHtml.utils = {
+		// Functions for getting various types of values from HTML attributes
+		getObjFromAttr: function(node, attr){
+			// used to for e.g. formatter, get, renderCell
+			var val = node.getAttribute(attr);
+			//console.log('getObjFromAttr:', node, attr, ' => ', val);
+			return val && lang.getObject(val);
+		},
+		getBoolFromAttr: function(node, attr){
+			// used for e.g. sortable
+			var val = node.getAttribute(attr);
+			return val && val !== "false";
+		},
+		getNumFromAttr: function(node, attr){
+			// used for e.g. rowSpan, colSpan
+			var val = node.getAttribute(attr);
+			val = val && Number(val);
+			return isNaN(val) ? undefined : val;
+		},
+		
+		// Function for aggregating th attributes into column properties
+		getColumnFromCell: function(th){
+			var
+				getObj = GridFromHtml.utils.getObjFromAttr,
+				getBool = GridFromHtml.utils.getBoolFromAttr,
+				getNum = GridFromHtml.utils.getNumFromAttr;
+			
+			return {
+				name: th.innerHTML,
+				field: th.getAttribute("field") || th.className || th.innerHTML,
+				className: th.className,
+				sortable: getBool(th, "sortable"),
+				get: getObj(th, "get"),
+				formatter: getObj(th, "formatter"),
+				renderCell: getObj(th, "renderCell"),
+				rowSpan: getNum(th, "rowspan"),
+				colSpan: getNum(th, "colspan")
+			}
+		}
+	}
+	return GridFromHtml;
 });
