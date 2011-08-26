d47567564f835d211a9da7c9e427a6440f43951e
Chris Barrett <cbarrett@sitepen.com>
2011-08-26 17:12:29 -0500

    i hate sort

--- Grid.js
+++ Grid.js
@@ -18,7 +18,6 @@ define(["dojo/has", "put-selector/put", "dojo/_base/declare", "dojo/on", "./Edit
 				return this.cell(target).column;
 			}
 		},
-		listType: "grid",
 		cell: function(target, columnId){
 			// summary:
 			//		Get the cell object by node, or event, id, plus a columnId
