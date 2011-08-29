846638d173602b436490d719d559e694e283793c
Kris Zyp <kriszyp@gmail.com>
2011-08-29 14:54:32 -0600

    Update docs on selection

--- doc/Selection.js
+++ doc/Selection.js
@@ -6,7 +6,7 @@ function(create, List){
 		{
 			cellSelection: Boolean("Indicates that selection should take place at the cell level instead of the row level", false),
 			selectionMode: String("Indicates which selection mode to use. The options include 'none' (clicks and keyboard do not select anything), 'single' (only the last clicked item is selected), 'multiple' (each click adds a selected item), and 'extended' (clicks select last item, but ctrl and shift can be used to select multiple)", "extended"),
-			selection: Object("This is an instance of a dojo.Stateful object representing the selected items where the property keys of the object correspond to the object ids of the selected objects. One can watch() the selected object to monitor for changes to the selected set. If cell level selection is enabled, each property value will be an object hash with property keys corresponding to the column ids."),
+			selection: Object("This is an object representing the selected items where the property keys of the object correspond to the object ids of the selected objects. One can watch() the selected object to monitor for changes to the selected set. If cell level selection is enabled, each property value will be an object hash with property keys corresponding to the column ids."),
 			select: Method("Select an item", {
 				id: String("Id of the object to select"),
 				columnId: String("This argument (it is optional) may be included to indicate which cell in a row to select (otherwise the whole row is selected)")
