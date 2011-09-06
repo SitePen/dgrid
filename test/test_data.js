b463d4bbfaaddc6439a4c5d5c86b446f4166674e
Chris Barrett <cbarrett@sitepen.com>
2011-09-06 16:44:10 -0500

    making changes for performance tests

--- test/test_data.js
+++ test/test_data.js
@@ -39,6 +39,24 @@ define(["dojo/store/Memory", "dojo/store/Observable"],function(Memory, Observabl
 	}
 	// global var testTypesStore
 	testTypesStore = Observable(Memory({data: typesData}));
+
+	var perfData = [];
+	for(var i = 0; i < 20000; i++){
+		perfData.push({
+			id: i,
+			integer: Math.floor(Math.random() * 100),
+			floatNum: Math.random() * 100,
+			date: new Date(new Date().getTime() * Math.random() * 2),
+			date2: new Date(new Date().getTime() - Math.random() * 1000000000),
+			text: "A number in text " + Math.random(),
+			bool: Math.random() > 0.5,
+			bool2: Math.random() > 0.5,
+			price: Math.random() * 100,
+			today: new Date(new Date().getTime())
+		});
+	}
+	// global var testPerfStore
+	testPerfStore = new Memory({data: perfData});
 	
 	// global var testCountryStore
 	testCountryStore = Observable(new Memory({
