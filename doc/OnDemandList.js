aeb00b048cdbc6bfece7035bd4b07cfc6fc5b3b2
Kris Zyp <kriszyp@gmail.com>
2011-09-12 16:14:47 -0600

    Add some more comments and docs on node removal

--- doc/OnDemandList.js
+++ doc/OnDemandList.js
@@ -5,6 +5,8 @@ function(create, List){
 		"The OnDemandList module renders objects from an object store, using a store's query method and using the store's paging and sorting capabilities for efficiency. This component employs virtual scrolling/paging to pull in data as the list is scrolled.",
 		{
 			store: Object("The object store that will act as the data provider for the list"),
+			farOffRemoval: Number("Rows can be removed if they are this distance in pixels from the visible viewing area. Set this to infinity if you never want rows removed"),
+			maxEmptySpace: Number("This defines the maximum size (in pixels) of the empty, not-yet-rendered space below the rendered rows."),
 			query: Union(["string", "object"], "The query to use to query the store"),
 			queryOptions: Object("The query options to provide on the queries"),
 			minRowsPerPage: Number("The minimum number of rows to page in at once"),
