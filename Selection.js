5388e40034c7053a1eccc39677f2b02136cb6565
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2011-09-20 14:04:25 -0400

    Reduce number of console.logs (some might still be useful ATM)

--- Selection.js
+++ Selection.js
@@ -21,30 +21,24 @@ return declare([List], {
 				if(!event._selected && (event.type == "mousedown" || !event.ctrlKey || event.keyCode == 32)){
 					event._selected = true;
 					var row = event.target;
-					console.log("event: ", event);
-					console.log("in focus");
+					console.log("in focus; event: ", event, "; row: ", row);
 					if(mode == "single" && lastRow && event.ctrlKey){
-						console.log("1");
 						grid.deselect(lastRow);
 						if(lastRow == row){
 							return;
 						}
 					}
 					if(!event.ctrlKey){
-						console.log("mode: ", mode);
 						if(mode != "multiple"){
 							grid.clearSelection();
 						}
 						grid.select(row);
 					}else{
-						console.log("4");
 						grid.select(row, null, null);
 					}
 					if(event.shiftKey && mode != "single"){
-						console.log("5");
 						grid.select(lastRow, row);
 					}else{
-						console.log("row: ", row);
 						lastRow = row;
 					}
 					if(event.type == "mousedown" && (event.shiftKey || event.ctrlKey)){
