12097c21a7649851613225d2b8d89646caa8c746
Bryan Forbes <bryan@sitepen.com>
2011-10-18 16:21:44 -0500

    Use aspect.around to modify grid's _handleSelect method.

--- Selector.js
+++ Selector.js
@@ -1,4 +1,4 @@
-define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put"], function(on, aspect, has, put){
+define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put", "dojo/query"], function(on, aspect, has, put, query){
 	return function(column, type){
 		// accept arguments as parameters to Selector function, or from column def
 		column.type = type = type || column.type;
@@ -6,32 +6,50 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put"], funct
 
 		var grid;
 		function onSelect(event){
-			if(!event._selected && (!event.ctrlKey || event.keyCode == 32)){
-				var row = grid.row(event),
-					value = null;
-				if(type == "radio"){
-					grid.clearSelection();
-					value = true;
-				}
-
-				if(row){
-					grid.select(row.id, null, value);
-				}else if(type != "radio"){
-					put(this, (grid.allSelected ? "!" : ".") + "dgrid-select-all");
-					grid[grid.allSelected ? "clearSelection" : "selectAll"]();
-				}
-				event._selected = true;
+			if(event.type == "cellfocusin" && (event.parentType == "mousedown" || event.keyCode != 32)){
+				// ignore "cellfocusin" from "mousedown" and any keystrokes other than spacebar
 				return;
+			}
+			var row = grid.row(event),
+				lastRow = grid._lastRow,
+				value = null;
+
+			if(type == "radio"){
+				grid.clearSelection();
+				value = true;
+			}
+			if(row){
+				grid.select(row.id, null, value);
+			}else if(type != "radio"){
+				put(this, (grid.allSelected ? "!" : ".") + "dgrid-select-all");
+				grid[grid.allSelected ? "clearSelection" : "selectAll"]();
+			}
+			if(event.shiftKey && lastRow){
+				grid.select(lastRow, row);
+			}else if(row){
+				grid._lastRow = row.element;
 			}
 		}
 
 		function setupSelectionEvents(){
 			// register one listener at the top level that receives events delegated
-			grid._hasSelectionInputListener = true;
+			grid._hasSelectorInputListener = true;
+			aspect.around(grid, "_handleSelect", function(_handleSelect){
+				return function(event, currentTarget){
+					var target = event.target;
+					while(!query.matches(target, ".dgrid-selector-cell", grid.contentNode)){
+						if(target == grid.contentNode || !(target = target.parentNode)){
+							break;
+						}
+					}
+					if(!target || target == grid.contentNode){
+						_handleSelect.call(this, event);
+					}else{
+						onSelect.call(target, event);
+					}
+				};
+			});
 			aspect.before(grid, "_initSelectionEvents", function(){
-				// Add listeners for these events before Selection adds its own
-				// so we can short-circuit what Selection will do
-				on(this.contentNode, ".dgrid-selector-cell:mousedown,.dgrid-selector-cell:cellfocusin", onSelect);
 				on(this.headerNode, ".dgrid-selector-cell:mousedown,.dgrid-selector-cell:cellfocusin", onSelect);
 			});
 		}
@@ -41,14 +59,13 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put"], funct
 				tabIndex: isNaN(column.tabIndex) ? -1 : column.tabIndex
 			}));
 
-			if(!grid._hasSelectionInputListener){
+			if(!grid._hasSelectorInputListener){
 				setupSelectionEvents();
 			}
 
 			return input;
 		};
 
-		var rowRE = /\bdgrid-row\b/;
 		column.renderCell = function(object, value, cell, options, header){
 			if(!grid){
 				grid = column.grid;
@@ -59,7 +76,7 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put"], funct
 
 			if(header && (type == "radio" || typeof object == "string")){
 				cell.appendChild(document.createTextNode(object||""));
-				if(!grid._hasSelectionInputListener){
+				if(!grid._hasSelectorInputListener){
 					setupSelectionEvents();
 				}
 			}else{
