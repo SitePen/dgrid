c9a0357070522f7cc19db91a00e0cb2635034d3f
Bryan Forbes <bryan@sitepen.com>
2011-10-21 15:05:11 -0500

    Remove unneeded parameter.

--- Selector.js
+++ Selector.js
@@ -14,7 +14,7 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put", "dojo/
 
 			if(type == "radio"){
 				if(!lastRow || lastRow.id != row.id){
-					grid.clearSelection(lastRow);
+					grid.clearSelection();
 					grid.select(row, null, true);
 					grid._lastSelected = row.element;
 				}
