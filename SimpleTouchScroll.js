63c81567a8c3327935e67e7f7229d16bfab162fc
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2011-10-30 15:44:28 -0400

    SimpleTouchScroll: improve multitouch detection

--- SimpleTouchScroll.js
+++ SimpleTouchScroll.js
@@ -1,20 +1,34 @@
 define(["dojo/_base/declare", "dojo/on"],
 function(declare, on){
-	var startPos; // holds coords of touchstart
+	var startPos, // records coords of touchstart
+		touches = 0; // records number of touches on document
+	
+	function updatetouchcount(evt){
+		touches = evt.touches.length;
+	}
+	
 	function ontouchstart(evt){
-		var t = evt.touches[0];
+		var t;
+		// check "global" touches count (which hasn't counted this touch yet)
+		if(touches > 0){ return; } // ignore multitouch gestures
+		
+		t = evt.touches[0];
 		startPos = [this.scrollLeft + t.pageX, this.scrollTop + t.pageY];
 	}
 	function ontouchmove(evt){
-		var t = evt.touches[0];
-		// don't interfere with multitouch gestures
-		if(evt.touches.length != 1){ return; }
+		var t;
+		if(touches > 1){ return; } // ignore multitouch gestures
+		
+		t = evt.touches[0];
 		// snuff event and scroll the area
 		evt.preventDefault();
 		evt.stopPropagation();
 		this.scrollLeft = startPos[0] - t.pageX;
 		this.scrollTop = startPos[1] - t.pageY;
 	}
+	
+	// hook up touch listeners to entire body to track number of active touches
+	on(document.body, "touchstart,touchend,touchcancel", updatetouchcount);
 	
 	return declare([], {
 		startup: function(){
