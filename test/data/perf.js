cf20951589f6cdfc850dffcd40627fb3a0413d86
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2011-11-07 15:55:38 -0500

    Make data/perf.js generate data more efficiently

--- test/data/perf.js
+++ test/data/perf.js
@@ -1,17 +1,17 @@
 define(["dojo/store/Memory", "dojo/store/Observable"],function(Memory, Observable){
-	var perfData = [];
+	var perfData = [], today = new Date().getTime();
 	for(var i = 0; i < 20000; i++){
 		perfData.push({
 			id: i,
 			integer: Math.floor(Math.random() * 100),
 			floatNum: Math.random() * 100,
-			date: new Date(new Date().getTime() * Math.random() * 2),
-			date2: new Date(new Date().getTime() - Math.random() * 1000000000),
+			date: new Date(today * Math.random() * 2),
+			date2: new Date(today - Math.random() * 1000000000),
 			text: "A number in text " + Math.random(),
 			bool: Math.random() > 0.5,
 			bool2: Math.random() > 0.5,
 			price: Math.random() * 100,
-			today: new Date(new Date().getTime())
+			today: new Date(today)
 		});
 	}
 	return new Memory({data: perfData});
