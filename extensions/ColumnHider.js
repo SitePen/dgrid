6b069e73f97aa12ff93a53807a34ee90dfa9f2f8
Dean Landolt <dean@deanlandolt.com>
2011-12-17 10:13:53 -0500

    change unhidable to unhideable

--- extensions/ColumnHider.js
+++ extensions/ColumnHider.js
@@ -97,7 +97,7 @@ define(["dojo/_base/declare", "dojo/has", "dojo/on", "dojo/query", "dojo/dom", "
 			for(id in this.columns){
 				col = this.columns[id];
 				// allow cols to opt out of the hider (specifically for selector col)
-				if (col.unhidable) continue;
+				if (col.unhideable) continue;
 				
 				// create the HTML for each column selector.
 				div = put(hiderMenuNode, "div.dgrid-hider-menu-row");
