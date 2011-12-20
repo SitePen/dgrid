5b2c78ab8d1a378da252834ed810065341eaaa64
Colin Snover <github.com@zetafleet.com>
2011-12-20 22:12:59 +0000

    Update Dojo to 1.7.1 and remove other packages to attempt submoduling.

--- dgrid/doc/OnDemandGrid.js
+++
@@ -1,7 +0,0 @@
-define(["./create-schema", "./OnDemandList", "./Grid"], 
-function(create, OnDemandList, Grid){
-	with(create){
-	return Constructor([OnDemandList, Grid], 
-		"The OnDemandGrid module combines the store-driven virtual capabilities of the OnDemandList with the tabular data view of the Grid");
-	}
-});
