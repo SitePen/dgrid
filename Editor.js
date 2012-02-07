b7f538bba49ea8157ca0c48b93d06005ddddb744
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2012-02-06 19:26:07 -0500

    Fix issues in refactor RE enter/esc and datachange firing reliably

--- Editor.js
+++ Editor.js
@@ -1,10 +1,10 @@
 define(["dojo/_base/kernel", "dojo/on", "dojo/has", "dojo/_base/lang", "./Grid", "put-selector/put", "dojo/_base/sniff"],
 function(kernel, on, has, lang, Grid, put){
 
-var activeValues = [], // used to track active field's original/new values
-	ignoreChange = false; // used to ignore change immediately after esc/enter
+var ignoreChange = false; // used to ignore change on native input after esc/enter
 
 function setProperty(grid, cellElement, oldValue, value){
+	// Updates dirty hash and fires dgrid-datachange event for a changed value.
 	if(oldValue != value){
 		var cell = grid.cell(cellElement);
 		var row = cell.row;
@@ -20,6 +20,7 @@ function setProperty(grid, cellElement, oldValue, value){
 				value = isNaN(asDate.getTime()) ? value : asDate;
 			}
 			// TODO: remove rowId in lieu of cell (or grid.row/grid.cell)
+			// (keeping for the moment for back-compat, but will note in changes)
 			if(on.emit(cellElement, "dgrid-datachange", {
 						cell: cell,
 						rowId: row.id,
@@ -45,39 +46,9 @@ function setProperty(grid, cellElement, oldValue, value){
 function setPropertyFromWidget(grid, widget, value) {
 	// This function serves as a window into setProperty for widget editors.
 	if(!widget.isValid || widget.isValid()){ // only update if valid
-		if(activeValues[1] !== undefined){
-			activeValues[1] = setProperty(
-				grid, widget.domNode.parentNode, activeValues[1], value);
-		}
+		widget._dgridlastvalue = setProperty(
+			grid, widget.domNode.parentNode, widget._dgridlastvalue, value);
 	}
-}
-
-// general event logic
-
-function changeHandler(grid, evt){
-	// event handler triggered on change of common input elements
-	var target = evt.target;
-	if(ignoreChange){ return;	}
-	
-	if(activeValues[0] !== undefined && target.className.indexOf("dgrid-input") > -1){
-		activeValues[1] = setProperty(grid, target.parentNode, activeValues[1],
-			target[target.type == "checkbox" || target.type == "radio"  ? "checked" : "value"]);
-	}
-}
-
-function dismissOnKey(column, evt){
-	// Contains logic for reacting to enter/escape keypresses to save/cancel edits.
-	// Returns boolean specifying whether this key event should dismiss the field.
-	var key = evt.keyCode || evt.which;
-	
-	if (key == 27){ // escape: revert + dismiss
-		activeValues[1] = activeValues[0];
-		ignoreChange = true;
-		return true;
-	}else if (key == 13 && column.dismissOnEnter !== false){ // enter: dismiss
-		return true;
-	}
-	return false; // don't dismiss
 }
 
 // editor creation/hookup/placement logic
@@ -89,7 +60,7 @@ function createEditor(column){
 		editOn = column.editOn,
 		grid = column.grid,
 		isWidget = typeof editor != "string", // string == standard HTML input
-		args, cmp, node, className, putstr;
+		args, cmp, node, className, putstr, handleChange;
 	
 	// TODO: deprecate widgetArgs in favor of editorArgs
 	// (leaving open the possibility of using it for HTML textarea, etc)
@@ -97,42 +68,34 @@ function createEditor(column){
 	if(typeof args == "function"){ args = args.call(grid, column); }
 	
 	if(isWidget){
-		
 		cmp = new editor(args);
 		
 		// Add dgrid-input to className to make consistent with HTML inputs.
 		// (Can't do this using className argument to constructor; causes issues)
 		(cmp.focusNode || cmp.domNode).className += " dgrid-input";
 		
-		if(editOn){
-			// connect to onBlur if we're deactivating on it anyway; watching on value
-			// will fire too late due to several setTimeouts in Dijit
-			cmp.connect(cmp, "onBlur", function(){
-				setPropertyFromWidget(grid, this, this.get("value"));
-			});
-		}else{
-			// if we're not deactivating on blur, it's safe/prudent to use watch
-			cmp.watch("value", function(key, oldValue, value){
-				setPropertyFromWidget(grid, this, value);
-			});
-		}
+		// connect to onBlur rather than watching value for changes, since
+		// the latter is delayed by setTimeouts and may also fire from our logic
+		cmp.connect(cmp, "onBlur", function(){
+			setPropertyFromWidget(grid, this, this.get("value"));
+		});
 	}else{
+		handleChange = function(evt){
+			var target = evt.target;
+			if(ignoreChange){ return;	}
+			
+			if("_dgridlastvalue" in target && target.className.indexOf("dgrid-input") > -1){
+				target._dgridlastvalue = setProperty(grid, target.parentNode, target._dgridlastvalue,
+					target[target.type == "checkbox" || target.type == "radio"  ? "checked" : "value"]);
+			}
+		};
+
 		// considerations for standard HTML form elements
 		if(!column.grid._hasInputListener){
 			// register one listener at the top level that receives events delegated
 			grid._hasInputListener = true;
-			grid.on("change", function(evt){ changeHandler(grid, evt); });
-		}
-		if(has("ie") < 9){
-			// IE<9 doesn't fire change events for all the right things,
-			// and it doesn't bubble.
-			// TODO: test IE9 quirks
-			if(editor == "radio" || editor == "checkbox"){
-				// listen for clicks since IE doesn't fire change events properly for checks/radios
-				on(cmp, "click", function(evt){ changeHandler(grid, evt); });
-			}else{
-				on(cmp, "change", function(evt){ changeHandler(grid, evt); });
-			}
+			grid.on("change", function(evt){ handleChange(evt); });
+			// also register a focus listener
 		}
 		
 		putstr = editor == "textarea" ? "textarea" :
@@ -141,6 +104,19 @@ function createEditor(column){
 			name: column.field, // TODO: item id?
 			tabIndex: isNaN(column.tabIndex) ? -1 : column.tabIndex
 		}, args));
+		
+		if(has("ie") < 9){
+			// IE<9 doesn't fire change events for all the right things,
+			// and it doesn't bubble.
+			// TODO: test IE9 quirks
+			// FIXME: blowing up here!
+			if(editor == "radio" || editor == "checkbox"){
+				// listen for clicks since IE doesn't fire change events properly for checks/radios
+				on(cmp, "click", function(evt){ handleChange(evt); });
+			}else{
+				on(cmp, "change", function(evt){ handleChange(evt); });
+			}
+		}
 	}
 	
 	return cmp;
@@ -153,29 +129,42 @@ function createSharedEditor(column, originalRenderCell){
 	var cmp = createEditor(column),
 		isWidget = cmp.domNode,
 		node = cmp.domNode || cmp,
-		parentNode,
-		stopper, keyHandler, blurHandler,
-		onblur = function(){
-			// FIXME: currently having problems due to blur being multi-fired
-			// (Wasn't an issue previously because the listener was already unhooked)
-			
-			// force FF/IE to fire onchange first when fired from esc/enter
-			node.blur();
-			
-			// remove the editor from the cell
-			parentNode = node.parentNode;
-			parentNode.removeChild(node);
-			
-			// pass new value to original renderCell implementation for this cell
-			//TODO: test; is the cost of grid.row() worth hooking this centrally?
-			//(maybe expand info about active editor instead?)
-			originalRenderCell(column.grid.row(parentNode).data, activeValues[1],
-				parentNode);
-			
-			// reset state
+		focusNode = cmp.focusNode || node,
+		reset = isWidget ?
+			function(){ cmp.set("value", cmp._dgridlastvalue); } :
+			function(){ cmp.value = cmp._dgridlastvalue; },
+		stopper, keyHandle, blurHandle;
+	
+	function onblur(){
+		var parentNode = node.parentNode;
+		
+		// FIXME: currently having problems due to blur being multi-fired
+		// (Wasn't an issue previously because the listener was already unhooked)
+		
+		// remove the editor from the cell
+		parentNode.removeChild(node);
+		
+		// pass new value to original renderCell implementation for this cell
+		//TODO: test; is the cost of grid.row() worth hooking this centrally?
+		//(maybe expand info about active editor instead?)
+		originalRenderCell(column.grid.row(parentNode).data, cmp._dgridlastvalue,
+			parentNode);
+	}
+	
+	function dismissOnKey(evt){
+		// Contains logic for reacting to enter/escape keypresses to save/cancel edits.
+		// Returns boolean specifying whether this key event should dismiss the field.
+		var key = evt.keyCode || evt.which;
+		
+		if(key == 27){ // escape: revert + dismiss
+			ignoreChange = true;
+			reset();
+			focusNode.blur();
 			ignoreChange = false;
-			activeValues = [];
-		};
+		}else if(key == 13 && column.dismissOnEnter !== false){ // enter: dismiss
+			focusNode.blur();
+		}
+	}
 	
 	// don't allow event to confuse grid when editor is already active
 	// TODO: delegate from bodyNode via `.column-<id> .dgrid-input`?
@@ -185,21 +174,18 @@ function createSharedEditor(column, originalRenderCell){
 	});
 	
 	// hook up enter/esc key handling
-	// TODO: test for various input widgets - is domNode good enough?
-	keyHandler = on(node, "keydown", function(evt){
-		dismissOnKey(column, evt) && onblur();
-	});
+	keyHandle = on(focusNode, "keydown", dismissOnKey);
 	
 	if(isWidget){
 		// need to further wrap blur callback, to check for validity first,
 		// and to add a timeout to avoid throwing errors for key events after blur
 		// (TODO: verify if the timeout is still needed since we no longer destroy)
-		cmp.connect(cmp, "onBlur", function(){
+		blurHandle = on.pausable(cmp, "blur", function(){
 			if(cmp.isValid && !cmp.isValid()){ return; }
 			onblur();
 		});
 	}else{
-		blurHandler = on(node, "blur", onblur);
+		blurHandle = on.pausable(node, "blur", onblur);
 	}
 	
 	return cmp;
@@ -219,6 +205,8 @@ function showEditor(cmp, column, value, cell, object){
 		cmp.value = value;
 		if(editor == "radio" || editor == "checkbox"){ cmp.checked = !!value; }
 	}
+	// track previous value for short-circuiting or in case we need to revert
+	cmp._dgridlastvalue = value;
 	
 	cell.innerHTML = "";
 	put(cell, cmp.domNode || cmp);
@@ -261,8 +249,6 @@ return function(column, editor, editOn){
 				var cmp = column.editorInstance;
 				if(!column.canEdit || column.canEdit(object, value)){
 					showEditor(cmp, column, value, cell, object);
-					// initialize old and new value for active field
-					activeValues = [value, value];
 					// focus the newly-placed control
 					cmp.focus && cmp.focus(); // supported by form widgets and HTML inputs
 				}
