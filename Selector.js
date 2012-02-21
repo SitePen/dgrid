5829e873c998c804be49fec2c6808b9ed0d82e56
Kris Zyp <kriszyp@gmail.com>
2012-02-21 15:34:31 -0700

    Override allowSelect to disable selection for disabled selectors. Remove errant ui-icon class. Set indeterminate on header checkbox when mixed selection occurs. Handle keydown events so we can workaround FF's lack of access to shiftKey bug on click events Set input checked property on shiftKey to ensure proper resulting state Reverse order of to and from rows in range to keep proper lastInput value

--- Selector.js
+++ Selector.js
@@ -1,31 +1,79 @@
-define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put", "dojo/query"], function(on, aspect, has, put, query){
+define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put"], function(on, aspect, has, put){
 	return function(column, type){
 		// accept arguments as parameters to Selector function, or from column def
 		column.type = type = type || column.type;
 		column.sortable = false;
 		
-		var grid, recentInput, recentTimeout;
+		var grid, recentInput, recentTimeout, headerCheckbox;
+		function changeInput(value){
+			// creates a function that modifies the input on an event
+			return function(event){
+				var element = grid.cell(event.row, column.id).element;
+				element = (element.contents || element).input;
+				if(!element.disabled){
+					// only change the value if it is not disabled
+					element.checked = value;
+				}
+				// see if the header checkbox needs to be indeterminate
+				var mixed = false;
+				var selection = grid.selection; 
+				for(var i in selection){
+					// if there is anything in the selection, than it is indeterminate
+					if(selection[i] != grid.allSelected){
+						mixed = true;
+						break;
+					}
+				}
+				headerCheckbox.indeterminate = mixed;
+				headerCheckbox.checked = grid.allSelected;
+				
+				// we record the most recent event to avoid undoing it in the next event (click)
+				recentInput = element;
+				clearTimeout(recentTimeout);
+				recentTimeout = setTimeout(function(){
+					// the most intuitive way to determine if the next event should be 
+					// cancelled seems more to do with time than any sequence of events,
+					// after a short pause a user naturally beings to expect that their
+					// next action (releasing the mouse) will have an affect. 
+					recentInput = false;
+				}, 500);
+			};
+		}
+
 		function onSelect(event){
 			if(recentInput == this){
 				// an event recently occurred on this input so we cancel it so we don't have the click undoing the selection
 				return event.preventDefault();
 			}
-			var row = grid.row(event), lastRow = grid._lastSelected && grid.row(grid._lastSelected);
-
-			if(type == "radio"){
-				if(!lastRow || lastRow.id != row.id){
-					grid.clearSelection();
-					grid.select(row, null, true);
-					grid._lastSelected = row.element;
-				}
-			}else{
-				if(row){
-					lastRow = event.shiftKey ? lastRow : null;
-					grid.select(row, lastRow||null, lastRow ? undefined : null);
-					grid._lastSelected = grid.selectionMode != "single" && row.element;
+			// we would really only care about click, since other input sources, like spacebar
+			// trigger a click, but the click event doesn't provide access to the shift key in firefox, so
+			// listen for keydown's as well to get an event in firefox that we can properly retrieve
+			// the shiftKey property from
+			if(event.type == "click" || event.keyCode == 32){ 
+				var row = grid.row(event), lastRow = grid._lastSelected && grid.row(grid._lastSelected);
+	
+				if(type == "radio"){
+					if(!lastRow || lastRow.id != row.id){
+						grid.clearSelection();
+						grid.select(row, null, true);
+						grid._lastSelected = row.element;
+					}
 				}else{
-					put(this, (grid.allSelected ? "!" : ".") + "dgrid-select-all");
-					grid[grid.allSelected ? "clearSelection" : "selectAll"]();
+					if(row){
+						if(event.shiftKey){
+							// make sure the last input always ends up checked for shift key 
+							changeInput(true)({row: row});
+						}else{
+							// no shift key, so no range selection
+							lastRow = null;
+						}
+						lastRow = event.shiftKey ? lastRow : null;
+						grid.select(lastRow|| row, row, lastRow ? undefined : null);
+						grid._lastSelected = grid.selectionMode != "single" && row.element;
+					}else{
+						put(this, (grid.allSelected ? "!" : ".") + "dgrid-select-all");
+						grid[grid.allSelected ? "clearSelection" : "selectAll"]();
+					}
 				}
 			}
 		}
@@ -34,29 +82,12 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put", "dojo/
 			// register one listener at the top level that receives events delegated
 			grid._hasSelectorInputListener = true;
 			aspect.before(grid, "_initSelectionEvents", function(){
-				this.on(".dgrid-selector-input:click", onSelect);
+				this.on(".dgrid-selector-input:click,.dgrid-selector-input:keydown", onSelect);
 			});
-			function changeInput(value){
-				// creates a function that modifies the input on an event
-				return function(event){
-					var element = grid.cell(event.row, column.id).element;
-					element = (element.contents || element).input;
-					if(!element.disabled){
-						// only change the value if it is disabled
-						element.checked = value;
-					}
-					if(value){
-						// we record the most recent event to avoid undoing it in the next event (click)
-						recentInput = element;
-						clearTimeout(recentTimeout);
-						recentTimeout = setTimeout(function(){
-							// the most intuitive way to determine if the next event should be 
-							// cancelled seems more to do with time than any sequence of events,
-							// after a short pause a user naturally beings to expect that their
-							// next action (releasing the mouse) will have an affect. 
-							recentInput = false;
-						}, 500);
-					}
+			if(typeof column.disabled == "function"){
+				var originalAllowSelect = grid.allowSelect;
+				grid.allowSelect = function(row){
+					return originalAllowSelect.call(this, row) && !column.disabled(row.data);
 				};
 			}
 			// register listeners to the select and deselect events to change the input checked value
@@ -66,7 +97,7 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put", "dojo/
 		
 		var disabled = column.disabled;
 		var renderInput = typeof type == "function" ? type : function(value, cell, object){
-			var input = cell.input || (cell.input = put(cell, "input.ui-icon.dgrid-selector-input[type="+type + "]", {
+			var input = cell.input || (cell.input = put(cell, "input.dgrid-selector-input[type="+type + "]", {
 				tabIndex: isNaN(column.tabIndex) ? -1 : column.tabIndex,
 				disabled: disabled && (typeof disabled == "function" ? disabled(object) : disabled),
 				checked: value,
@@ -97,9 +128,9 @@ define(["dojo/on", "dojo/aspect", "dojo/_base/sniff", "put-selector/put", "dojo/
 				renderInput(value, cell, object);
 			}
 		};
-
 		column.renderHeaderCell = function(th){
 			column.renderCell(column.label || {}, null, th, null, true);
+			headerCheckbox = th.lastChild;
 		};
 
 		return column;
