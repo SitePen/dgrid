SitePen/dgrid

commit e2a4cd708d0d3d6d91e9b07f4afeec1eac6baf44

Remove row wrapping when pressing left/right to navigate cells.

List.js
@@ -61,17 +61,14 @@ function(put, declare, listen, aspect, has, TouchScroll, hasClass){
 		steps = steps || 1;
 		do{
 			// move in the correct direction
-			if(nextSibling = current[steps < 0 ? 'previousSibling' : 'nextSibling']){
-				do{
-					current = nextSibling;
-					if(((current && current.className) + ' ').indexOf(targetClass + ' ') > -1){
-						// it's an element with the correct class name, counts as a real move
-						element = current;
-						steps += steps < 0 ? 1 : -1;
-						break;
-					}
-					// if the next sibling isn't a match, drill down to search
-				}while(nextSibling = current[steps < 0 ? 'lastChild' : 'firstChild']);
+			if((nextSibling = current[steps < 0 ? 'previousSibling' : 'nextSibling'])){
+				current = nextSibling;
+				if(((current && current.className) + ' ').indexOf(targetClass + ' ') > -1){
+					// it's an element with the correct class name, counts as a real move
+					element = current;
+					steps += steps < 0 ? 1 : -1;
+					break;
+				}
 			}else if((current = current.parentNode) == this.domNode){ // intentional assignment
 				// we stepped all the way out of the grid, given up now
 				break;

commit c3f2cd92c817b5e1b4aef74f329971d8becdd046

Provide a distinction between traversing through all nodes (used by selection), and just visible nodes (used by keyboard). Also fix navigation for entering into nested elements (for tree navigation), which was broken by https://github.com/SitePen/dgrid/commit/e2a4cd708d0d3d6d91e9b07f4afeec1eac6baf44

Keyboard.js
@@ -174,7 +174,7 @@ return declare([List], {
 					cell = grid.row(cellFocusedElement);
 				}
 				if(move){
-					cell = cell && grid[orientation](cell, move);
+					cell = cell && grid[orientation](cell, move, true);
 				}
 				var nextFocus = cell && cell.element;
 				if(nextFocus){

List.js
@@ -54,20 +54,24 @@ function(arrayUtil, kernel, declare, listen, aspect, has, miscUtil, TouchScroll,
 		return document.getElementById(id);
 	}
 
-	function move(item, steps, targetClass){
+	function move(item, steps, targetClass, visible){
 		var nextSibling, current, element;
 		element = current = item.element;
 		steps = steps || 1;
 		do{
 			// move in the correct direction
-			if((nextSibling = current[steps < 0 ? 'previousSibling' : 'nextSibling'])){
-				current = nextSibling;
-				if(((current && current.className) + ' ').indexOf(targetClass + ' ') > -1){
-					// it's an element with the correct class name, counts as a real move
-					element = current;
-					steps += steps < 0 ? 1 : -1;
-				}
-			}else if((current = current.parentNode) == this.domNode){ // intentional assignment
+			if(nextSibling = current[steps < 0 ? 'previousSibling' : 'nextSibling']){
+				do{
+					current = nextSibling;
+					if(((current && current.className) + ' ').indexOf(targetClass + ' ') > -1){
+						// it's an element with the correct class name, counts as a real move
+						element = current;
+						steps += steps < 0 ? 1 : -1;
+						break;
+					}
+					// if the next sibling isn't a match, drill down to search
+				}while(nextSibling = (!visible || current.style && current.style.display != "none") && current[steps < 0 ? 'lastChild' : 'firstChild']);
+			}else if((current = current.parentNode) == this.domNode || (current.className + ' ').indexOf("dgrid-row ") > -1){ // intentional assignment
 				// we stepped all the way out of the grid, given up now
 				break;
 			}
@@ -506,11 +510,11 @@ function(arrayUtil, kernel, declare, listen, aspect, has, miscUtil, TouchScroll,
 			};
 		},
 		_move: move,
-		up: function(row, steps){
-			return this.row(move(row, -(steps || 1), "dgrid-row"));
+		up: function(row, steps, visible){
+			return this.row(move(row, -(steps || 1), "dgrid-row", visible));
 		},
-		down: function(row, steps){
-			return this.row(move(row, steps || 1, "dgrid-row"));
+		down: function(row, steps, visible){
+			return this.row(move(row, steps || 1, "dgrid-row", visible));
 		},
 		
 		get: function(/*String*/ name /*, ... */){