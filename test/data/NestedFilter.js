e340fc5b1391587d99a67fcaff5e8cc523ea0e0c
Kris Zyp <kriszyp@gmail.com>
2012-05-31 16:23:29 -0600

    Updated NestedFilter to be more robust and not rely on queryEngine

--- test/data/NestedFilter.js
+++ test/data/NestedFilter.js
@@ -13,35 +13,43 @@ function(lang, QueryResults, Deferred, DeferredList){
 				if(!query){
 					return results;
 				}
-				var parent =options.parent;
-				var newStore = this, newResults = [], deferreds = [];
-				return new QueryResults(Deferred.when(results.forEach(function(object){
-					if(store.queryEngine(query)([object]).length > 0){
+				var newStore = this, 
+					newResults = [], 
+					matchedIds = {}, 
+					deferreds = [
+					// query for items that match the query, these will go through with any filters on the children
+					store.query(lang.mixin({parent: options.parent}, query), options).forEach(function(object){
 						// if this object matches the query, we can immediately return it without checking children,
 						// and it will be a full object without any filtering applied to it's children
-						newResults.push(object);
-					}else{
-						var hasChild;
-						// check to see if any of the children match the query
-						deferreds.push(newStore.getChildren(object, options, query).forEach(function(child){
-							// if one of the children matches, we return a modified object that
-							// denotes that it doesn't have all it's children, just the ones the match the query
-							if(!hasChild){
-								newResults.push(lang.delegate(object, {
-									_query: query
-								}));
-								hasChild = true;
-							}
-						}));
-					}
+						var id = store.getIdentity(object);
+						var index = matchedIds[id];
+						// if it was added by the filtered query results below, replace with the full version, otherwise just add it
+						newResults.splice(isNaN(index) ? newResults.length : index, 1, object);
+						matchedIds[id] = -1;
+					})];
+				return new QueryResults(Deferred.when(results.forEach(function(object){
+					var hasChild;
+					var id = store.getIdentity(object);
+					
+					// check to see if any of the children match the query
+					deferreds.push(newStore.getChildren(object, options, query).forEach(function(child){
+						// if one of the children matches, we return a modified object that
+						// denotes that it doesn't have all it's children, just the ones the match the query
+						if(isNaN(matchedIds[id])){ // only add it has not already been added
+							matchedIds[id] = newResults.push(lang.delegate({
+								_query: query
+							}, object)) - 1;
+						}
+					}));
 				}), function(){
 					// once done we return the new results as a query results
 					return new DeferredList(deferreds).then(function(){
 						return newResults;
 					});
-				}));			
+				}));
 			},
 			getChildren: function(parent, options, query){
+				// overide to use the object's query if it has one
 				options.parent = parent.id;
 				return this.query(query || parent._query, options);
 			},
