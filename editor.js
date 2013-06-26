cd2ffaba8b9df87766f0db6be648c002a397d078
Martin Repta <mrepta@gratex.com>
2013-06-26 12:52:15 +0200

    Fix #603: prevent WebKit from throwing DOM Exception when editor lost focus

--- editor.js
+++ editor.js
@@ -220,7 +220,11 @@ function createSharedEditor(column, originalRenderCell){
 			cancelable: false
 		});
 		// Remove the editor from the cell, to be reused later.
-		parentNode.removeChild(node);
+		try{
+			parentNode.removeChild(node);
+		}catch(e){
+			// empty catch block; prevent WebKit from throwing DOM Exception 8
+		}
 		
 		put(cell.element, "!dgrid-cell-editing");
 		
