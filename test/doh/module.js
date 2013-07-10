69369912b749b8f72f5a1e90167a52e669275f79
Nick Nisi <nnisi@sitepen.com>
2013-07-09 20:04:05 -0500

    Fix incorrect file name in doh test

--- test/doh/module.js
+++ test/doh/module.js
@@ -2,5 +2,5 @@ define(["doh", "require", "./_StoreMixin" ], function(doh, require){
 	doh.register("create-destroy", require.toUrl("./create-destroy.html"));
 	doh.register("addCssRule", require.toUrl("./addCssRule.html"));
 	doh.register("stores", require.toUrl("./stores.html"));
-	doh.register("selectionRowUpdates", require.toUrl("./selectionRowUpdates.html"));
+	doh.register("selectionRowUpdates", require.toUrl("./Selection-updates.html"));
 });
