ebe7bd75a9cb83df3e06dd81fc43cf42e9e16704
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2013-07-29 08:37:45 -0400

    Fix #674: selector: Remove unnecessary check

--- selector.js
+++ selector.js
@@ -69,10 +69,6 @@ function(kernel, arrayUtil, on, aspect, has, put){
 				var row = grid.row(event),
 					lastRow = grid._lastSelected && grid.row(grid._lastSelected);
 
-				if (!grid.allowSelect(row)) {
-					return;
-				}
-
 				grid._selectionTriggerEvent = event;
 				
 				if(type == "radio"){
