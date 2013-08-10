eb33b1239d70ed255d190856d25ffee0b9dbbeae
Ed Hager <ehager@sitepen.com>
2013-08-09 17:44:47 -0700

    Committing my progress on cleaning up css rules from Grid.styleColumn.  Tests need to be written for ColumnSet.styleColumnSet.

--- test/intern/core/addCssRule.js
+++ test/intern/core/addCssRule.js
@@ -1,12 +1,14 @@
 define([
 	"intern!tdd",
 	"intern/assert",
+	"dojo/_base/lang",
+	"dojo/_base/array",
 	"dojo/dom-style",
 	"dojo/dom-construct",
 	"dojo/query",
 	"dgrid/Grid",
 	"dgrid/util/misc"
-], function (test, assert, domStyle, domConstruct, query, Grid, miscUtil) {
+], function (test, assert, lang, array, domStyle, domConstruct, query, Grid, miscUtil) {
 	var testDiv;
 
 	test.suite("addCssRule", function(){
@@ -103,19 +105,21 @@ define([
 
 		test.test("addCssRule via dgrid APIs", function(){
 			var values = ["7px", "8px"],
+				columns = {
+					name: "Name",
+					value: "Value",
+					comment: "Comment" // unstyled buffer
+				},
 				origValues,
-				grid;
+				grid,
+				rules,
+				i;
 			
-			function createGrid(cleanAddedRules){
-				grid = new Grid({
+			function createGrid(config){
+				grid = new Grid(lang.mixin({
 					id: "my.grid", // test escaping of CSS identifiers from methods
-					columns: {
-						name: "Name",
-						value: "Value",
-						comment: "Comment" // unstyled buffer
-					},
-					cleanAddedRules: cleanAddedRules
-				});
+					columns: columns
+				}, config || {}));
 				document.body.appendChild(grid.domNode);
 				grid.startup();
 			}
@@ -136,47 +140,111 @@ define([
 				];
 			}
 			
-			function checkRules(expected){
+			function checkRules(expected, extra){
 				var actual = getStyles(grid);
 				assert.strictEqual(expected[0], actual[0],
-					"Style modified via addCssRule has expected value");
+					(extra || "") + " Style modified via addCssRule has expected value. Expected: " + expected[0] + ", Actual: " + actual[0]);
 				assert.strictEqual(expected[1], actual[1],
-					"Style modified via styleColumn has expected value");
+					(extra || "") + " Style modified via styleColumn has expected value. Expected: " + expected[1] + ", Actual: " + actual[1]);
+			}
+
+			function removeRules(rules){
+				array.forEach(rules,function(rule){
+					rule.remove();
+				});
 			}
 			
-			// Create grid for the first time
-			createGrid(true);
+			// Test if rules are applied
+			createGrid({cleanAddedRules: true, cleanAddedColumnRules: true});
 			// Collect original style values for later cleanup check
 			origValues = getStyles();
 			// Add rules and make sure they applied as expected
 			addRules();
-			checkRules(values);
+			checkRules(values, "1:");
 			// Destroy the grid, which should remove the style rules
 			grid.destroy();
 			
-			// Create grid for the second time, with cleanAddedRules: false
-			createGrid(false);
+			// Create a grid and tell it not to clean rules added via addCssRule.
+			createGrid({cleanAddedRules: false, cleanAddedColumnRules: true});
 			// Before adding styles, make sure the ones from last time were removed
-			checkRules(origValues);
+			checkRules(origValues, "2:");
 			// Add rules and check again;
 			// store the rules for tearDown since they won't be cleaned up
-			this.rules = addRules();
-			checkRules(values);
-			// Destroy the grid, which should *not* remove the style rules
+			rules = addRules();
+			checkRules(values, "3:");
+			// Destroy the grid, which should *not* remove the addCssRule style rules
+			// but should remove the styleColumn rules
 			grid.destroy();
 			
-			// Create grid for the third time
-			createGrid(true);
-			// Check that styles from last time still exist
-			checkRules(values);
+			// Create a grid to see if the styles are still there.
+			createGrid({cleanAddedRules: true, cleanAddedColumnRules: true});
+			// Check that one style from last time still exists
+			checkRules([values[0], origValues[1]], "4:");
 			// Destroy the grid (which won't remove the styles from last time,
 			// since no handles were added to this exact instance)
 			grid.destroy();
 			// Clean up rule litter from cleanAddedRules: false test
-			var i;
-			for(i in this.rules){
-				this.rules[i].remove();
-			}
+			removeRules(rules);
+
+			// Create a grid and tell it not to clean up styles created from styleColumn
+			createGrid({cleanAddedRules: true, cleanAddedColumnRules: false});
+			// Before adding styles, make sure the ones from last time were removed
+			checkRules(origValues, "5:");
+			// Add rules and check again;
+			// store the rules for tearDown since they won't be cleaned up
+			rules = addRules();
+			checkRules(values, "6:");
+			// Destroy the grid, which should *not* remove the styleColumn rules
+			grid.destroy();
+
+			// Create a grid to see if the styleColumn styles are still there.
+			createGrid({cleanAddedRules: true, cleanAddedColumnRules: true});
+			// Check that one style from last time still exists
+			checkRules([origValues[0], values[1]], "7:");
+			// Destroy the grid (which won't remove the styles from last time,
+			// since no handles were added to this exact instance)
+			grid.destroy();
+			// Clean up rule litter from cleanAddedColumnRules: false test
+			removeRules(rules);
+
+			// Create a grid to see if setting the columns clears the styleColumn rules
+			createGrid();
+			// Before adding styles, make sure the ones from last time were removed
+			checkRules(origValues, "8:");
+			// Add rules and make sure they applied as expected
+			rules = addRules();
+			assert.strictEqual(1, grid._columnRules.length);
+			checkRules(values, "9:");
+			// Set the columns to the same definition.
+			grid.set("columns", columns);
+			// The column set rules should have reset.
+			checkRules([values[0], origValues[1]], "10:");
+			// Add rules and make sure they applied as expected
+			addRules();
+			assert.strictEqual(1, grid._columnRules.length);
+			checkRules(values, "11:");
+			// Destroy the grid, which should remove the style rules
+			grid.destroy();
+			createGrid();
+			checkRules(origValues, "12:");
+			grid.destroy();
+
+			// Create a grid to see if setting the columns does not clear the styleColumn rules
+			// when cleanAddedColumnRules is false;
+			createGrid({cleanAddedColumnRules: false});
+			// Before adding styles, make sure the ones from last time were removed
+			checkRules(origValues, "13:");
+			// Add rules and check again;
+			// store the rules for tearDown since they won't be cleaned up
+			rules = addRules();
+			checkRules(values, "14:");
+			// Set the columns to the same definition.
+			grid.set("columns", columns);
+			// The rules should be the same
+			checkRules(values, "15:");
+			// Destroy the grid, which should *not* remove the styleColumn rules
+			grid.destroy();
+			removeRules(rules);
 		});
 	});
 });
