6a0b6e1e610f6ddea1b34211df707c494eda7251
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2013-08-27 14:48:12 -0400

    Make Keyboard unit tests more specific and reduce repetition

--- test/intern/mixins/Keyboard.js
+++ test/intern/mixins/Keyboard.js
@@ -1,260 +1,213 @@
 define([
 	"intern!tdd",
 	"intern/assert",
-	"dgrid/OnDemandList", 
-	"dgrid/OnDemandGrid", 
-	"dgrid/Keyboard", 
-	"dojo/_base/declare", 
-	"dojo/on", 
-	"dojo/dom-construct",
-	"dojo/query", 
-	"put-selector/put", 
+	"dgrid/OnDemandList",
+	"dgrid/OnDemandGrid",
+	"dgrid/Keyboard",
+	"dojo/_base/declare",
+	"dojo/on",
+	"dojo/query",
+	"put-selector/put",
 	"dgrid/test/data/base"
-], function(test, assert, OnDemandList, OnDemandGrid, Keyboard, declare, on, domConstruct, query, put){
+], function(test, assert, OnDemandList, OnDemandGrid, Keyboard, declare, on, query, put){
 	var handles = [],
-		grid,
-		list,
-		getFirstTarget = function(grid, anchorClass){
-			return query(anchorClass + " .dgrid-cell", grid.domNode)[0] ||
-				query(anchorClass + " .dgrid-row", grid.domNode)[0];
-		};
+		columns = {
+			col1: "Column 1",
+			col3: "Column 3",
+			col5: "Column 5"
+		},
+		grid;
+	
+	function destroyGrid(){
+		grid.destroy();
+	}
+	
+	function removeHandles(){
+		for(var i = handles.length; i--;){
+			handles[i].remove && handles[i].remove();
+		}
+		handles = [];
+	}
+	
+	// Common test functions for grid w/ cellNavigation: false and list
+	
+	function testRowFocus(){
+		var rowId;
+		// listen for a dgrid-cellfocusin event
+		handles.push(on(document.body, "dgrid-cellfocusin", function(e){
+			assert.ok(e.row, "dgrid-cellfocusin event got a non-null row value");
+			rowId = e.row.id;
+		}));
+		// trigger a focus with no argument, which should focus the first row
+		grid.focus();
+		// XXX: dgrid/List#row returns string ID when looking up DOM node
+		assert.strictEqual(rowId, "0",
+			"dgrid-cellfocusin event triggered on first row on focus() call");
+	}
+	
+	function testRowFocusArgs(){
+		var rowId;
+		// listen for a dgrid-cellfocusin event
+		handles.push(on(document.body, "dgrid-cellfocusin", function(e){
+			assert.ok(e.row, "dgrid-cellfocusin event got a non-null row value");
+			rowId = e.row.id;
+		}));
+		// trigger a body focus with the second row as the target
+		grid.focus(query(".dgrid-row", grid.contentNode)[1]);
+		// make sure we got the right row
+		assert.strictEqual(rowId, "1",
+			"dgrid-cellfocusin event triggered on expected row");
+	}
+	
+	function testRowBlur(){
+		var blurredRow,
+			targets = query(".dgrid-row", grid.contentNode);
+		
+		// call one focus event, followed by a subsequent focus event, 
+		// thus triggering a dgrid-cellfocusout event
+		grid.focus(targets[0]);
+		
+		// listen for a dgrid-cellfocusout event
+		handles.push(on(document.body, "dgrid-cellfocusout", function(e){
+			blurredRow = e.row;
+			assert.ok(blurredRow, "dgrid-cellfocusout event got a non-null row value");
+		}));
+		
+		grid.focus(targets[1]);
+		// make sure our handler was called
+		assert.strictEqual(blurredRow && blurredRow.id, "0",
+			"dgrid-cellfocusout event triggered on expected row");
+	}
 
 	test.suite("Keyboard (Grid + cellNavigation:true)", function(){
 		test.before(function(){
-			var columns = {
-					col1: "Column 1",
-					col3: "Column 3",
-					col5: "Column 5"
-				},
-				KeyboardGrid = declare([OnDemandGrid, Keyboard]);
-			// create a grid
-			grid = new KeyboardGrid({
+			grid = new (declare([OnDemandGrid, Keyboard]))({
 				store: testStore,
 				columns: columns
 			});
-			// place it and start it up
-			domConstruct.place(grid.domNode, document.body, "first");
+			document.body.appendChild(grid.domNode);
 			grid.startup();
 		});
 
-		test.afterEach(function(){
-			for(var i=0, len=handles.length; i<len; i++){
-				handles[i].remove && handles[i].remove();
-			}
-		});
+		test.afterEach(removeHandles);
+		test.after(destroyGrid);
 
-		test.after(function(){
-			grid.destroy();
-		});
-
-		// Tests
 		test.test("grid.focus + no args", function(){
-			var focused;
-			// listen for a focus in event
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
-				focused = true;
-			}));
-			// trigger a focus
-			grid.focus();
-			// Make sure we are good
-			assert.strictEqual(focused, true, "dgrid-cellfocusin event triggered on cell focus");
-		});
-
-		test.test("grid.focusHeader + no args", function(){
-			var focused;
-			// listen for a focus in event (header triggers same event)
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
-				focused = true;
-			}));
-			// trigger a header focus
-			grid.focusHeader();
-			// Make sure we are good
-			assert.strictEqual(focused, true, "dgrid-cellfocusin event triggered on cell header focus");
-		});
-
-		test.test("grid.focus + args", function(){
-			var focusedId;
-			// listen for a focus in event
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
-				focusedId = e.cell.row.id;
-			}));
-			// trigger a header focus with first cell as the target
-			grid.focus(getFirstTarget(grid, ".dgrid-content"));
-			// make sure we got the right cell
-			assert.strictEqual(focusedId, "0", "dgrid-cellfocusin event triggered on cell focus");
-		});
-
-		test.test("grid.focusHeader + args", function(){
 			var colId;
-			// listen for a focus in event
+			// listen for a dgrid-cellfocusin event
 			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
 				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
 				colId = e.cell.column.id;
 			}));
+			// trigger a focus with no argument, which should focus the first cell
+			grid.focus();
+			assert.strictEqual(colId, "col1",
+				"dgrid-cellfocusin event triggered on first cell on focus() call");
+		});
+
+		test.test("grid.focusHeader + no args", function(){
+			var colId;
+			// listen for a dgrid-cellfocusin event (header triggers same event)
+			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
+				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
+				assert.ok(!e.row, "dgrid-cellfocusin event for header got a falsy row value");
+				colId = e.cell.column.id;
+			}));
+			// trigger a header focus with no argument, which should focus the first cell
+			grid.focusHeader();
+			assert.strictEqual(colId, "col1",
+				"dgrid-cellfocusin event triggered on first cell on focusHeader() call");
+		});
+
+		test.test("grid.focus + args", function(){
+			var focusedCell;
+			// listen for a dgrid-cellfocusin event
+			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
+				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
+				focusedCell = e.cell;
+			}));
+			// trigger a body focus with the second cell as the target
+			grid.focus(query(".dgrid-cell", grid.contentNode)[1]);
+			// make sure our handler was called appropriately
+			assert.ok(focusedCell, "dgrid-cellfocusin event fired");
+			assert.strictEqual(focusedCell.row.id, "0",
+				"dgrid-cellfocusin event triggered on expected row");
+			assert.strictEqual(focusedCell.column.id, "col3",
+				"dgrid-cellfocusin event triggered on second cell on focus(...) call");
+		});
+
+		test.test("grid.focusHeader + args", function(){
+			var colId;
+			// listen for a dgrid-cellfocusin event (header triggers same event)
+			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
+				assert.ok(e.cell, "dgrid-cellfocusin event got a non-null cell value");
+				assert.ok(!e.row, "dgrid-cellfocusin event for header got a falsy row value");
+				colId = e.cell.column.id;
+			}));
 			// trigger a focus on the first header cell
-			grid.focus(getFirstTarget(grid, ".dgrid-header"));
+			grid.focus(query(".dgrid-cell", grid.headerNode)[1]);
 			// make sure we got the right header cell
-			assert.strictEqual(colId, "col1", "dgrid-cellfocusin event triggered on cell header focus");
+			assert.strictEqual(colId, "col3", "dgrid-cellfocusin event triggered on expected header cell");
 		});
 
 		test.test("dgrid-cellfocusout event", function(){
-			var focusedOut;
-			// listen for a focus OUT event
-			handles.push(on(document.body, "dgrid-cellfocusout", function(e){
-				assert.ok(e.cell, "dgrid-cellfocusout event got a non-null cell value");
-				focusedOut = true;
-			}));
+			var blurredCell,
+				targets = query(".dgrid-cell", grid.contentNode);
+			
 			// call one focus event, followed by a subsequent focus event, 
 			// thus triggering a dgrid-cellfocusout event
-			grid.focus(getFirstTarget(grid, ".dgrid-content"));
-			grid.focus(getFirstTarget(grid, ".dgrid-content"));
-			// make sure our handler was called
-			assert.strictEqual(focusedOut, true, "dgrid-cellfocusout event triggered on subsequent cell focuses");
+			grid.focus(targets[0]);
+			
+			// listen for a dgrid-cellfocusout event
+			handles.push(on(document.body, "dgrid-cellfocusout", function(e){
+				blurredCell = e.cell;
+				assert.ok(blurredCell, "dgrid-cellfocusout event got a non-null cell value");
+			}));
+			
+			grid.focus(targets[1]);
+			// make sure our handler was called appropriately
+			assert.ok(blurredCell, "dgrid-cellfocusout event fired");
+			assert.strictEqual(blurredCell.row.id, "0",
+				"dgrid-cellfocusout event triggered on expected row");
+			assert.strictEqual(blurredCell.column.id, "col1",
+				"dgrid-cellfocusout event triggered on expected column");
 		});
 	});
 
 	test.suite("Keyboard (Grid + cellNavigation:false)", function(){
 		test.before(function(){
-			var columns = {
-					col1: "Column 1",
-					col3: "Column 3",
-					col5: "Column 5"
-				},
-				KeyboardGrid = declare([OnDemandGrid, Keyboard]);
-			// create a grid
-			grid = new KeyboardGrid({
+			grid = new (declare([OnDemandGrid, Keyboard]))({
 				store: testStore,
 				columns: columns,
 				cellNavigation: false
 			});
-			// place it and start it up
-			domConstruct.place(grid.domNode, document.body, "first");
+			document.body.appendChild(grid.domNode);
 			grid.startup();
 		});
 
-		test.afterEach(function(){
-			for(var i=0, len=handles.length; i<len; i++){
-				handles[i].remove && handles[i].remove();
-			}
-		});
+		test.afterEach(removeHandles);
+		test.after(destroyGrid);
 
-		test.after(function(){
-			grid.destroy();
-		});
-
-		// Tests
-		test.test("grid.focus + no args", function(){
-			var focused;
-			// listen for a focus in event
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.row, "dgrid-cellfocusin event got a non-null row value");
-				focused = true;
-			}));
-			// trigger a focus
-			grid.focus();
-			// Make sure we are good
-			assert.strictEqual(focused, true, "dgrid-cellfocusin event triggered on row focus");
-		});
-
-		test.test("grid.focus + args", function(){
-			var focusedId;
-			// listen for a focus in event
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.row, "dgrid-cellfocusin event got a non-null row value");
-				focusedId = e.row.id;
-			}));
-			// trigger a header focus with first row as the target
-			grid.focus(getFirstTarget(grid, ".dgrid-content"));
-			// make sure we got the right row
-			assert.strictEqual(focusedId, "0", "dgrid-cellfocusin event triggered on row focus");
-		});
-
-		test.test("dgrid-cellfocusout event", function(){
-			var focusedOut;
-			// listen for a focus OUT event
-			handles.push(on(document.body, "dgrid-cellfocusout", function(e){
-				assert.ok(e.row, "dgrid-cellfocusout event got a non-null row value");
-				focusedOut = true;
-			}));
-			// call one focus event, followed by a subsequent focus event, 
-			// thus triggering a dgrid-cellfocusout event
-			grid.focus(getFirstTarget(grid, ".dgrid-content"));
-			grid.focus(getFirstTarget(grid, ".dgrid-content"));
-			// make sure our handler was called
-			assert.strictEqual(focusedOut, true, "dgrid-cellfocusout event triggered on subsequent row focuses");
-		});
+		test.test("grid.focus + no args", testRowFocus);
+		test.test("grid.focus + args", testRowFocusArgs);
+		test.test("dgrid-cellfocusout event", testRowBlur);
 	});
 
 	test.suite("Keyboard (List)", function(){
 		test.before(function(){
-			var columns = {
-					col1: "Column 1",
-					col3: "Column 3",
-					col5: "Column 5"
-				},
-				KeyboardList = declare([OnDemandList, Keyboard]);
-			// create a list
-			list = new KeyboardList({
+			grid = new (declare([OnDemandList, Keyboard]))({
 				store: testStore,
 				renderRow: function(item){ return put("div", item.col5); }
 			});
-			// place it and start it up
-			domConstruct.place(list.domNode, document.body, "first");
-			list.startup();
+			document.body.appendChild(grid.domNode);
+			grid.startup();
 		});
 
-		test.afterEach(function(){
-			for(var i=0, len=handles.length; i<len; i++){
-				handles[i].remove && handles[i].remove();
-			}
-		});
+		test.afterEach(removeHandles);
+		test.after(destroyGrid);
 
-		test.after(function(){
-			list.destroy();
-		});
-
-		test.test("list.focus + no args", function(){
-			var focused;
-			// listen for a focus in event
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.row, "dgrid-cellfocusin event got a non-null cell value");
-				focused = true;
-			}));
-			// trigger a focus
-			list.focus();
-			// Make sure we are good
-			assert.strictEqual(focused, true, "dgrid-cellfocusin event triggered on row focus");
-		});
-
-		test.test("list.focus + args", function(){
-			var focusedId;
-			// listen for a focus in event
-			handles.push(on(document.body, "dgrid-cellfocusin", function(e){
-				assert.ok(e.row, "dgrid-cellfocusin event got a non-null cell value");
-				focusedId = e.row.id;
-			}));
-			// trigger a header focus with first cell as the target
-			list.focus(getFirstTarget(list, ".dgrid-content"));
-			// make sure we got the right cell
-			assert.strictEqual(focusedId, "0", "dgrid-cellfocusin event triggered on cell focus");
-		});
-
-		test.test("dgrid-cellfocusout event", function(){
-			var focusedOut;
-			// listen for a focus OUT event
-			handles.push(on(document.body, "dgrid-cellfocusout", function(e){
-				assert.ok(e.row, "dgrid-cellfocusout event got a non-null cell value");
-				focusedOut = true;
-			}));
-			// call one focus event, followed by a subsequent focus event, 
-			// thus triggering a dgrid-cellfocusout event
-			list.focus(getFirstTarget(list, ".dgrid-content"));
-			list.focus(getFirstTarget(list, ".dgrid-content"));
-			// make sure our handler was called
-			assert.strictEqual(focusedOut, true, "dgrid-cellfocusout event triggered on subsequent cell focuses");
-		});
+		test.test("list.focus + no args", testRowFocus);
+		test.test("list.focus + args", testRowFocusArgs);
+		test.test("dgrid-cellfocusout event", testRowBlur);
 	});
 });
