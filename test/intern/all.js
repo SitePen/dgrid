a83d5ab2d3b6e0e8066708e2477990a54ec34eec
Ed Hager <ehager@sitepen.com>
2014-01-16 16:29:46 -0800

    Create unit test suite for testing dojo/store/Observable
    This suite generates various test cases involving add, remove, and put
    operations across page boundaries on Observable stores.  It includes both
    single- and multiple-page cases, and tests with queryRowsOverlap as well.

--- test/intern/all.js
+++ test/intern/all.js
@@ -3,12 +3,13 @@ define([
 	'intern/node_modules/dojo/has!host-browser?./core/addCssRule',
 	'intern/node_modules/dojo/has!host-browser?./core/setClass',
 	'intern/node_modules/dojo/has!host-browser?./core/columns',
-	'intern/node_modules/dojo/has!host-browser?./mixins/Keyboard',
-	'intern/node_modules/dojo/has!host-browser?./mixins/Selection',
 	'intern/node_modules/dojo/has!host-browser?./core/stores',
 	'intern/node_modules/dojo/has!host-browser?./core/_StoreMixin',
+	'intern/node_modules/dojo/has!host-browser?./core/observable',
 	'intern/node_modules/dojo/has!host-browser?./core/OnDemand-removeRow',
 	'intern/node_modules/dojo/has!host-browser?./plugins/editor',
 	'intern/node_modules/dojo/has!host-browser?./plugins/tree-expand-promise',
-	'intern/node_modules/dojo/has!host-browser?./extensions/Pagination'
+	'intern/node_modules/dojo/has!host-browser?./extensions/Pagination',
+	'intern/node_modules/dojo/has!host-browser?./mixins/Keyboard',
+	'intern/node_modules/dojo/has!host-browser?./mixins/Selection'
 ], function(){});
