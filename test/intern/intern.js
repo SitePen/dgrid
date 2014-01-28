826f70b45a65d7e286f0b8019f035b2bd53e7dd5
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2014-01-28 09:36:02 -0500

    Update version dependencies in Intern config

--- test/intern/intern.js
+++ test/intern/intern.js
@@ -16,16 +16,19 @@ define({
 		'max-duration': 180,
 		// Increase timeout if Sauce Labs receives no new commands
 		// (no commands are sent during non-functional unit tests)
-		'idle-timeout': 180
+		'idle-timeout': 180,
+		// Specify Selenium version (the default is several versions old)
+		'selenium-version': '2.39.0'
 	},
 
 	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
+		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '21', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
+		{ browserName: 'firefox', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
 		{ browserName: 'chrome', platform: [ 'Linux', 'OS X 10.8', 'Windows 7' ] },
 		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
 	],
