f285e9702b99b244eb5eb38dae9a36e7de6a97d9
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2014-02-02 19:48:22 +0530

    Add: functions to set position of the selecor widgets and public variables to use and modify Selector Widgets. Modify: scope of functions and variables. Code Clean up and Add: documentation

--- extensions/advanceSelection.js
+++ extensions/advanceSelection.js
@@ -4,19 +4,31 @@ define(["dojo/_base/declare", "dojo/html", "dojo/has", "dojo/dom-construct", "di
 	 *	Advance Row Selection plugin for dgrid
 	 *	Originally contributed by RCorp(Ramanan Corporation, India) 2013-11-27
 	 *
-	 *  Just add plugin and use
 	 */
 
 	var invalidClassChars = /[^\._a-zA-Z0-9-]/g;
 	var contentBoxSizing = has("ie") < 8 && !has("quirks");
 	return declare(null, {
 		constructor: function() {
+			// selectAllButton: widget Button
+			//		Button widget to select All
+			this.selectAllButton = '';
 
+			// hiderMenuNode: widget Button
+			//		Button widget to de-select All.
+			this.selectNoneButton = '';
+
+			// hiderMenuNode: widget Button
+			//		Button widget to inverse selection.
+			this.selectInverseButton = '';
+
+			// seletorNode: DOMNode
+			//		The node for the placement of selection Buttons.
+			this.selectorNode = '';
 		},
-		/**
-		 * override the Grid's renderHeader function
-		 */
 		renderHeader: function() {
+			// summary:
+			// override the Grid's renderHeader function
 			this.inherited(arguments);
 			headerNode = this.headerNode
 
@@ -47,19 +59,19 @@ define(["dojo/_base/declare", "dojo/html", "dojo/has", "dojo/dom-construct", "di
 			headerNode.appendChild(row);
 			var trTag = ''
 			if ((has("ie") < 9 || has("quirks"))) {
-				// because of tBody Tag get chldren of children
+				// because of tBody Tag, get children of children
 				trTag = row.children[0].children[0]
 			} else {
 				trTag = row.children[0];
 			}
-			// trTag represents the row - containing 3 cells
+			this.selectorNode = trTag;
 
 			// first Cell
-			this.addSelectAllButton(trTag.children[0]);
+			this._createSelectAllButton();
 			// second Cell
-			this.addSelectNoneButton(trTag.children[1]);
+			this._createSelectNoneButton();
 			// third Cell
-			this.addSelectInverseButton(trTag.children[2]);
+			this._createSelectInverseButton();
 			// place row on top of the hedader-node
 			domConstruct.place(row, headerNode, 0);
 		},
@@ -88,16 +100,16 @@ define(["dojo/_base/declare", "dojo/html", "dojo/has", "dojo/dom-construct", "di
 			// // add the td to the tr at the end for better performance
 
 			// Create Cell for selecAll
-			tr.appendChild(this.addButton(tag, "selectAll"));
+			tr.appendChild(this._addButton(tag, "selectAll"));
 			// Create Cell for selecNone
-			tr.appendChild(this.addButton(tag, "selectNone"));
+			tr.appendChild(this._addButton(tag, "selectNone"));
 			// Create Cell for selecInverse
-			tr.appendChild(this.addButton(tag, "selectInverse"));
+			tr.appendChild(this._addButton(tag, "selectInverse"));
 			return row;
 		},
-		addButton: function(tag, id) {
+		_addButton: function(tag, id) {
 			// summary:
-			//		Create cell for button placement
+			// 		Create cells for Selector Buttons
 			var cell = put(tag + (".dgrid-cell.dgrid-cell-padding" + (id ? ".dgrid-column-" + id : "")).replace(invalidClassChars, "-") + "[role=" + (tag === "th" ? "columnheader" : "gridcell") + "]");
 			// diffrent id string for buttons cells
 			cell.id = id + '_Button_Cell';
@@ -110,44 +122,62 @@ define(["dojo/_base/declare", "dojo/html", "dojo/has", "dojo/dom-construct", "di
 			}
 			return cell;
 		},
-		addSelectAllButton: function(parentDiv) {
+		setPositionSelectorWidget: function(button, index) {
+			// summary:
+			//		Set Button Position
+			// index: integer
+			//		position of the button in selectorNode
+			// button: selector button widget
+			this.selectorNode.children[index].appendChild(button.domNode)
+		},
+		setPositionAllSelectorWidgets: function(posSelectAll, posSelectNone, posSelectInverse) {
+			// summary:
+			//		set position of  all selector buttons 
+			// posSelectAll: integer
+			//		position of the Select All button in selectorNode
+			// posSelectNone: integer
+			//		position of the Select None button in selectorNode
+			// posSelectInverse: integer
+			//		position of the Select Inverse button in selectorNode
+			// 
+			this.selectorNode.children[posSelectAll].appendChild(this.selectAllButton.domNode, posSelectAll)
+			this.selectorNode.children[posSelectNone].appendChild(this.selectNoneButton.domNode, posSelectNone)
+			this.selectorNode.children[posSelectInverse].appendChild(this.selectInverseButton.domNode, posSelectInverse)
+		},
+		_createSelectAllButton: function() {
+			// summary:
+			//		Button widget to select All rows
 			var _this = this;
-			/**
-			 * Button widget to select All rows
-			 * @type {Button}
-			 */
-			var selectAll = new Button({
+			this.selectAllButton = new Button({
 				label: "Select All",
 			});
-			selectAll.on('click', function(evt) {
+			this.selectAllButton.on('click', function(evt) {
 				_this.selectAll();
 			})
-			parentDiv.appendChild(selectAll.domNode)
+			// by default Place Select all at first cell
+			this.setPositionSelectorWidget(this.selectAllButton, 0)
 		},
-		addSelectNoneButton: function(parentDiv) {
+		_createSelectNoneButton: function() {
+			// summary:
+			//		Button widget to de-select All rows
 			var _this = this;
-			/**
-			 * Button widget to deselect All rows
-			 * @type {Button}
-			 */
-			var selectNone = new Button({
+			this.selectNoneButton = new Button({
 				label: "Select None",
 			});
-			selectNone.on('click', function(evt) {
+			this.selectNoneButton.on('click', function(evt) {
 				_this.clearSelection();
 			})
-			parentDiv.appendChild(selectNone.domNode)
+			// by default Place Select None at second cell
+			this.setPositionSelectorWidget(this.selectNoneButton, 1)
 		},
-		addSelectInverseButton: function(parentDiv) {
+		_createSelectInverseButton: function() {
+			// summary:
+			//		Button widget to toggle selection of rows
 			var _this = this;
-			/**
-			 * Button widget to toggle selection of rows
-			 * @type {Button}
-			 */
-			var selectInverse = new Button({
+			this.selectInverseButton = new Button({
 				label: "Select Inverse",
 			});
-			selectInverse.on('click', function() {
+			this.selectInverseButton.on('click', function() {
 				if (_this.allSelected) {
 					_this.clearSelection();
 				} else {
@@ -163,7 +193,8 @@ define(["dojo/_base/declare", "dojo/html", "dojo/has", "dojo/dom-construct", "di
 					_this._fireSelectionEvents();
 				}
 			})
-			parentDiv.appendChild(selectInverse.domNode)
+			// by default Place Select Inverse at third cell
+			this.setPositionSelectorWidget(this.selectInverseButton, 2)
 		}
 	});
 });
