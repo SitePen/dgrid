804c5631baab97c69444bfd2a520fdfdcbc1842a
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2014-02-02 19:37:32 +0530

    Updated:documentation

--- extensions/ColumnResizer.js
+++ extensions/ColumnResizer.js
@@ -88,10 +88,10 @@ function resizeColumnWidth(grid, colId, width, parentType){
 		if(width === "auto"){
 			delete column.width;
 		}else{
-			// while using columnResizer and advanceRowSelection while resizing column
-			// it gives an error Cannot set property 'width' of undefined,
-			// So check if column is defined then update column width.
-			// As it consoder columns created for placing Select All/Inverse/None Buttons
+			// while using columnResizer and advanceRowSelection together, on resizing
+			// columns it gives an error Cannot set property 'width' of undefined,
+			// Added if condition when column is undefined then not to update column width.
+			// This extension consider columns created for placing Select All/Inverse/None Buttons
 			// as grid column because they all share same class name. But we don't need
 			// to make them resizable atleast for now.
 			if(column) {
