a442724c7c16aabf6560384de9924b37f38f8693
Ed Hager <ehager@sitepen.com>
2014-05-06 14:53:12 -0700

    Tree test cleanup
    * Add tree tests to functional test module
    * Remove some duplication for renderExpando test
    * Lint/formatting fixes
    * Change the dgrid module ids back to absolute rather than relative
    * Simplify the createGrid function a bit
    * Skip the functional tests in Safari because mouseMoveTo is not supported

--- test/intern/functional/tree.js
+++ test/intern/functional/tree.js
@@ -2,23 +2,28 @@ define([
 	"intern!tdd",
 	"intern/chai!assert",
 	"require"
-], function (test, assert, require) {
+], function(test, assert, require){
 	// dgrid.css defines a 300ms transition duration for .dgrid-tree-container
 	// Add 20% for good measure
 	var TRANSITION_DELAY = 360;
 
-	function createExpandTest(clickTarget, clickMethod) {
-		return function () {
+	function createExpandTest(clickTarget, clickMethod){
+		return function(){
 			var xOffset = 0;
 			// Nudge the y-offset of the cursor down a bit for good measure (default seems to be 0,0 in the target
 			// element; in Firefox the click has been known to hit just above the target element)
 			var yOffset = 8;
 			var remote = this.get("remote");
 
+			if(remote.environmentType.browserName === "safari"){
+				console.warn("Warning: skipping a tree functional test because the safari web driver does not support mouseMoveTo.");
+				return;
+			}
+
 			// With the cell selector used in the double-click test the cursor will be positioned at the start of the
 			// cell, right over the expando
 			// Move the cursor a bit to the right so it's not on the expando
-			if (clickMethod === "doubleclick") {
+			if(clickMethod === "doubleclick"){
 				xOffset = 30;
 			}
 
@@ -26,21 +31,21 @@ define([
 				.moveTo(xOffset, 8)
 				[clickMethod]()
 				.sleep(TRANSITION_DELAY)
-			.end()
+				.end()
 				.elementByCssSelector("#treeGrid-row-SD")
 				.isDisplayed()
-				.then(function (isDisplayed) {
+				.then(function(isDisplayed){
 					assert.ok(isDisplayed, "Expanded rows should be visible");
 				})
-			.end()
+				.end()
 				.elementByCssSelector("#treeGrid-row-AF " + clickTarget)
 				.moveTo(xOffset, 8)
 				[clickMethod]()
 				.sleep(TRANSITION_DELAY)
-			.end()
+				.end()
 				.elementByCssSelector("#treeGrid-row-SD")
 				.isDisplayed()
-				.then(function (isDisplayed) {
+				.then(function(isDisplayed){
 					assert.ok(!isDisplayed, "Collapsed rows should not be visible");
 				});
 
@@ -48,8 +53,9 @@ define([
 		};
 	}
 
-	test.suite("dgrid/tree functional tests", function () {
-		test.before(function () {
+	test.suite("dgrid/tree functional tests", function(){
+
+		test.before(function(){
 			var remote = this.get("remote");
 
 			return remote.get(require.toUrl("./tree.html")).waitForCondition("ready", 15000);
--- test/intern/plugins/tree.js
+++ test/intern/plugins/tree.js
@@ -1,19 +1,20 @@
 define([
 	"intern!tdd",
 	"intern/chai!assert",
-	"../../../OnDemandGrid",
-	"../../../editor",
-	"../../../tree",
-	"../../../util/has-css3",
-	"../../../util/misc",
+	"dgrid/OnDemandGrid",
+	"dgrid/editor",
+	"dgrid/tree",
+	"dgrid/util/has-css3",
+	"dgrid/util/misc",
 	"dojo/_base/lang",
 	"dojo/_base/Deferred",
+	"dojo/dom-style",
 	"dojo/on",
 	"dojo/query",
 	"dojo/store/Memory",
 	"dojo/store/Observable",
 	"put-selector/put"
-], function(test, assert, OnDemandGrid, editor, tree, has, miscUtil, lang, Deferred, on, query, Memory, Observable, put){
+], function(test, assert, OnDemandGrid, editor, tree, has, miscUtil, lang, Deferred, domStyle, on, query, Memory, Observable, put){
 
 	var grid,
 		testDelay = 15,
@@ -62,29 +63,24 @@ define([
 			}
 		}));
 
-		if (options && options.useEditor) {
-			editorPlugin = editor;
-		}
-		else {
-			editorPlugin = function (columnDefinition) {
-				return columnDefinition;
-			}
-		}
-
 		treeColumnOptions = {
 			label: "id",
 			field: "id"
 		};
 
-		if (options && options.treeColumnOptions) {
-			treeColumnOptions = lang.delegate(treeColumnOptions, options.treeColumnOptions);
+		if(options && options.useEditor){
+			treeColumnOptions = editor(treeColumnOptions);
+		}
+
+		if(options && options.treeColumnOptions){
+			lang.mixin(treeColumnOptions, options.treeColumnOptions);
 		}
 
 		grid = new OnDemandGrid({
 			sort: "id",
 			store: store,
 			columns: [
-				tree(editorPlugin(treeColumnOptions)),
+				tree(treeColumnOptions),
 				{ label: "value", field: "value"}
 			]
 		});
@@ -205,7 +201,7 @@ define([
 					});
 			});
 
-			test.test("expand hidden", function() {
+			test.test("expand hidden", function(){
 				var dfd = this.async(1000);
 
 				grid.domNode.style.display = "none";
@@ -214,7 +210,7 @@ define([
 
 				// Since the grid is not displayed the expansion will occur without a transitionend event
 				// However, DOM updates from the expand will not complete within the current stack frame
-				setTimeout(dfd.callback(function() {
+				setTimeout(dfd.callback(function(){
 					var connected = grid.row(0).element.connected;
 					assert.isTrue(connected && connected.offsetHeight > 0,
 						"Node should be expanded with non-zero height");
@@ -227,24 +223,24 @@ define([
 			// * The store created in "createGrid" has a "mayHaveChildren" that returns true for nodes with no parentId
 			// * The expando icon (.dgrid-expando-icon) is always rendered, it is not visible if it lacks the class
 			//   ".ui-icon"
-			test.test("mayHaveChildren", function () {
+			test.test("mayHaveChildren", function(){
 				var rowObject;
 				var expandoNode;
 				var i;
 				var j;
 
-				for (i = 0; i < 5; i++) {
+				for(i = 0; i < 5; i++){
 					rowObject = grid.row(i);
 					expandoNode = query(".dgrid-expando-icon.ui-icon", rowObject.element)[0];
 					assert.isDefined(expandoNode, "Parent node should have an expando icon; node id = " + i);
 
 					grid.expand(i, true, true);
 
-					for (k = 0; k < 2; k++) {
+					for(k = 0; k < 2; k++){
 						rowObject = grid.row(i + ":" + k);
 						expandoNode = query(".dgrid-expando-icon.ui-icon", rowObject.element)[0];
 						assert.isUndefined(expandoNode,
-							"Child node should not have an expando icon; node id = " + i + ":" + k);
+								"Child node should not have an expando icon; node id = " + i + ":" + k);
 					}
 
 					grid.expand(i, false, true);
@@ -253,16 +249,16 @@ define([
 
 			// Test goal: ensure that rows are correctly expanded/collapsed on grid render in accordance with the
 			// column's "shouldExpand" method
-			test.test("shouldExpand", function () {
+			test.test("shouldExpand", function(){
 				var columns;
 				var shouldExpand;
 				var i;
 
 				columns = grid.get("columns");
-				columns[0].shouldExpand = function (rowObject) {
+				columns[0].shouldExpand = function(rowObject){
 					var shouldExpand = false;
 
-					if (rowObject.data.parentId === undefined) {
+					if(rowObject.data.parentId === undefined){
 						shouldExpand = !(rowObject.id % 2);
 					}
 
@@ -270,20 +266,20 @@ define([
 				};
 				grid.set("columns", columns);
 
-				for (i = 0; i < 5; i++) {
+				for(i = 0; i < 5; i++){
 					shouldExpand = !(i % 2);
 
-					if (shouldExpand) {
+					if(shouldExpand){
 						assert.isTrue(grid._expanded[i], "Row " + i + " should be expanded");
 					}
-					else {
+					else{
 						assert.isUndefined(grid._expanded[i], "Row " + i + " should not be expanded");
 					}
 				}
 			});
 
 			// Test goal: ensure that a custom "renderExpando" column method produces the expected DOM structure
-			test.test("renderExpando", function () {
+			test.test("renderExpando", function(){
 				var columns;
 				var rowObject;
 				var expandoNode;
@@ -291,34 +287,28 @@ define([
 				var i;
 
 				columns = grid.get("columns");
-				columns[0].renderExpando = function (level, hasChildren, expanded, object) {
-					// Slightly modified copy of dgrid's defaultRenderExpando method
+				columns[0].renderExpando = function(level, hasChildren, expanded, object){
+
 					// * Adds the "test-expando" class
 					// * Floats the expando at the opposite end of the cell
-					var dir = this.grid.isRTL ? "left" : "right",
-						cls = ".dgrid-expando-icon.test-expando",
-						node;
-					if(hasChildren){
-						cls += ".ui-icon.ui-icon-triangle-1-" + (expanded ? "se" : "e");
-					}
-					node = put("div" + cls + "[style=margin-" + dir + ": " +
-						(level * (this.indentWidth || 9)) + "px; float: " + dir + "]");
-					node.innerHTML = "&nbsp;"; // for opera to space things properly
+					var node = tree.defaultRenderExpando.apply(this, arguments);
+					put(node, ".test-expando");
+					domStyle.set(node, "float", "right");
 					return node;
 				};
 				grid.set("columns", columns);
 
-				for (i = 0; i < 5; i++) {
+				for(i = 0; i < 5; i++){
 					rowObject = grid.row(i);
 					expandoNode = query(".dgrid-expando-icon.ui-icon", rowObject.element)[0];
 					assert.isDefined(expandoNode, "Row " + i + " should have an expando icon");
 					assert.include(expandoNode.className, "test-expando",
-						"Row " + i + "'s expando icon should have the class \"test-expando\"");
+							"Row " + i + "'s expando icon should have the class \"test-expando\"");
 				}
 			});
 
 			// Test goal: ensure the expando node is still rendered when the column has a custom "renderCell" method
-			test.test("renderCell", function () {
+			test.test("renderCell", function(){
 				var rowObject;
 				var expandoNode;
 				var shouldExpand;
@@ -328,13 +318,13 @@ define([
 
 				createGrid({
 					treeColumnOptions: {
-						renderCell: function (rowObject, cellValue, cellNode) {
+						renderCell: function(rowObject, cellValue, cellNode){
 							return put("div.testRenderCell", cellValue);
 						}
 					}
 				});
 
-				for (i = 0; i < 5; i++) {
+				for(i = 0; i < 5; i++){
 					rowObject = grid.row(i);
 					expandoNode = query(".dgrid-expando-icon.ui-icon", rowObject.element)[0];
 					assert.isDefined(expandoNode, "Row " + i + " should have an expando node");
@@ -344,13 +334,13 @@ define([
 
 				createGrid({
 					treeColumnOptions: {
-						renderCell: function (rowObject, cellValue, cellNode) {
+						renderCell: function(rowObject, cellValue, cellNode){
 							put(cellNode, ".testRenderCell", cellValue);
 						}
 					}
 				});
 
-				for (i = 0; i < 5; i++) {
+				for(i = 0; i < 5; i++){
 					rowObject = grid.row(i);
 					expandoNode = query(".dgrid-expando-icon.ui-icon", rowObject.element)[0];
 					assert.isDefined(expandoNode, "Row " + i + " should have an expando node");
@@ -359,7 +349,7 @@ define([
 
 			// Test goal: ensure the expando node is still rendered with the editor plugin
 			// Note: ordering is important: tree(editor()), not editor(tree())
-			test.test("renderCell with editor", function () {
+			test.test("renderCell with editor", function(){
 				var rowObject;
 				var expandoNode;
 				var inputNode;
@@ -371,7 +361,7 @@ define([
 					useEditor: true
 				});
 
-				for (i = 0; i < 5; i++) {
+				for(i = 0; i < 5; i++){
 					rowObject = grid.row(i);
 					expandoNode = query(".dgrid-expando-icon.ui-icon", rowObject.element)[0];
 					assert.isDefined(expandoNode, "Row " + i + " should have an expando node");
@@ -380,7 +370,6 @@ define([
 				}
 			});
 		});
-
 
 		test.suite("tree + observable", function(){
 			test.beforeEach(createGrid);
