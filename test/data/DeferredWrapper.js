be5c20c690ff03322f31d1e84028a30975245fc7
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2014-05-28 13:56:47 -0700

    Fix DeferredWrapper utility module used for async store testing

--- test/data/DeferredWrapper.js
+++ test/data/DeferredWrapper.js
@@ -3,27 +3,30 @@ define(["dojo/_base/lang", "dojo/_base/Deferred"],function(lang, Deferred){
 	//		Creates a store that wraps the delegate store's query results and total in Deferred
 	//		instances. If delay is set, the Deferreds will be resolved asynchronously after delay +/-50%
 	//		milliseconds to simulate network requests that may come back out of order.
-	return function(store, delay){
+	return function (store, delay) {
 		return lang.delegate(store, {
-			fetch: function(){
-				if(!this.data || !this.data.then){
-					var results = store.fetch.call(this);
+			fetch: function () {
+				if (!this.data || !this.data.then) {
+					store.fetch.call(this);
+
+					var actualData = this.data;
+					var actualTotal = this.total;
 
 					var resultsDeferred = this.data = new Deferred();
 					var totalDeferred = this.total = new Deferred();
 
-					var resolveTotal = function(){
-						totalDeferred.resolve(results.length);
-					};
-					var resolveResults = function(){
-						resultsDeferred.resolve(results);
+					var resolveTotal = function () {
+						totalDeferred.resolve(actualTotal);
 					};
 
-					if(delay){
+					var resolveResults = function () {
+						resultsDeferred.resolve(actualData);
+					};
+
+					if (delay) {
 						setTimeout(resolveTotal, delay * (Math.random() + 0.5));
 						setTimeout(resolveResults, delay * (Math.random() + 0.5));
-					}
-					else{
+					} else {
 						resolveTotal();
 						resolveResults();
 					}
