8b837566e32abb704e87cb6d03453baac12961ef
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2014-07-10 09:03:16 -0400

    Further demo cleanup for 0.4
    dTuned:
    
    * Refactor data.js to data.json + RequestMemory
    * Re-flow as necessary to accommodate initial async query
    * Rename variables to make more sense
    * Make field names lowercase
    * Reformat according to new style guide
    
    multiview:
    
    * Separate store data to its own JSON resource and use RequestMemory
    * Use dojo/on for event handler registration
    * Reformat according to new style guide
    
    todo:
    
    * Remove a couple of superfluous statements/properties
    * Reformat according to new style guide

--- demos/dTuned/dTuned.js
+++ demos/dTuned/dTuned.js
@@ -1,68 +1,68 @@
 define([
-	"dgrid/List",
-	"dgrid/OnDemandGrid",
-	"dgrid/Selection",
-	"dgrid/Keyboard",
-	"dgrid/extensions/ColumnHider",
-	"dojo/_base/declare",
-	"dojo/_base/array",
-	"dojo/Stateful",
-	"dgrid/demos/dTuned/data",
-	"put-selector/put",
-	"dojo/domReady!"
-],
-function(List, Grid, Selection, Keyboard, Hider, declare, arrayUtil, Stateful, songStore, put){
+	'dgrid/List',
+	'dgrid/OnDemandGrid',
+	'dgrid/Selection',
+	'dgrid/Keyboard',
+	'dgrid/extensions/ColumnHider',
+	'dojo/_base/declare',
+	'dojo/_base/array',
+	'dojo/Stateful',
+	'dstore/RequestMemory',
+	'put-selector/put',
+	'dojo/domReady!'
+], function (List, Grid, Selection, Keyboard, Hider, declare, arrayUtil, Stateful, RequestMemory, put) {
 	// Create DOM
-	var headerNode = put("div#header");
-	var listNode = put("div#list-container");
-	var genresNode = put(listNode, "div#genres");
-	var artistsNode = put(listNode, "div#artists");
-	var albumsNode = put(listNode, "div#albums");
-	var gridNode = put("div#grid");
+	var headerNode = put('div#header');
+	var listNode = put('div#list-container');
+	var genresNode = put(listNode, 'div#genres');
+	var artistsNode = put(listNode, 'div#artists');
+	var albumsNode = put(listNode, 'div#albums');
+	var gridNode = put('div#grid');
+	var songStore = new RequestMemory({ target: 'data.json' });
 
-	put(document.body, headerNode, "div#header-content", "dTuned");
+	put(document.body, headerNode, 'div#header-content', 'dTuned');
 	put(document.body, listNode);
 	put(document.body, gridNode);
 	
 	// a formatting function for the Duration column.
-	function timeFormatter(t){
+	function timeFormatter(t) {
 		var tmp = parseInt(t, 10);
 		var min;
 		var sec;
 
-		if(isNaN(tmp)){
+		if (isNaN(tmp)) {
 			return t;
 		}
 
 		min = Math.floor(tmp / 60);
 		sec = tmp % 60;
 		// don't forget to pad seconds.
-		return "" + min + ":" + (sec < 10 ? "0" : "") + sec;
+		return '' + min + ':' + (sec < 10 ? '0' : '') + sec;
 	}
 	
-	function unique(arr){
+	function unique(arr) {
 		// Create a unique list of items from the passed array
 		// (removing duplicates).
 		var ret = [];
 
 		// First, set up a hashtable for unique objects.
 		var obj = {};
-		for(var i = 0, l = arr.length; i < l; i++){
-			if(!(arr[i] in obj)){
+		for (var i = 0, l = arr.length; i < l; i++) {
+			if (!(arr[i] in obj)) {
 				obj[arr[i]] = true;
 			}
 		}
 
 		// Now push the unique objects back into an array, and return it.
-		for(var p in obj){
+		for (var p in obj) {
 			ret.push(p);
 		}
 		ret.sort();
 		return ret;
 	}
 
-	function pickField(fieldName){
-		return function(object){
+	function pickField(fieldName) {
+		return function (object) {
 			return object[fieldName];
 		};
 	}
@@ -71,37 +71,43 @@ function(List, Grid, Selection, Keyboard, Hider, declare, arrayUtil, Stateful, s
 	var grid = new (declare([Grid, Selection, Keyboard, Hider]))({
 		collection: songStore,
 		columns: {
-			Name: "Name",
-			Time: { label: "Duration", formatter: timeFormatter },
-			Year: "Year",
-			Artist: "Artist",
-			Album: "Album",
-			Genre: "Genre"
+			name: 'Name',
+			time: { label: 'Duration', formatter: timeFormatter },
+			year: 'Year',
+			artist: 'Artist',
+			album: 'Album',
+			genre: 'Genre'
 		}
 	}, gridNode);
 
 	// define a List constructor with the features we want mixed in,
 	// for use by the three lists in the top region
-	var TunesList = declare([List, Selection, Keyboard]);
+	var TunesList = declare([List, Selection, Keyboard], {
+		selectionMode: 'single'
+	});
 
 	// define our three lists for the top.
-	var genres = new TunesList({ selectionMode: "single" }, genresNode);
-	var artists = new TunesList({ selectionMode: "single" }, artistsNode);
-	var albums = new TunesList({ selectionMode: "single" }, albumsNode);
+	var genresList = new TunesList({}, genresNode);
+	var artistsList = new TunesList({}, artistsNode);
+	var albumsList = new TunesList({}, albumsNode);
 
 	// create the unique lists and render them
-	var genreList = unique(arrayUtil.map(songStore.fetch(), pickField('Genre')));
-	var artistList = unique(arrayUtil.map(songStore.fetch(), pickField('Artist')));
-	var albumList = unique(arrayUtil.map(songStore.fetch(), pickField('Album')));
+	var genres, artists, albums;
+	
+	songStore.fetch().then(function (songs) {
+		genres = unique(arrayUtil.map(songs, pickField('genre')));
+		artists = unique(arrayUtil.map(songs, pickField('artist')));
+		albums = unique(arrayUtil.map(songs, pickField('album')));
+		
+		genres.unshift('All (' + genres.length + ' Genre' + (genres.length !== 1 ? 's' : '') + ')');
+		artists.unshift('All (' + artists.length + ' Artist' + (artists.length !== 1 ? 's' : '') + ')');
+		albums.unshift('All (' + albums.length + ' Album' + (albums.length !== 1 ? 's' : '') + ')');
 
-	genreList.unshift("All (" + genreList.length + " Genre" + (genreList.length !== 1 ? "s" : "") + ")");
-	artistList.unshift("All (" + artistList.length + " Artist" + (artistList.length !== 1 ? "s" : "") + ")");
-	albumList.unshift("All (" + albumList.length + " Album" + (albumList.length !== 1 ? "s" : "") + ")");
-
-	genres.renderArray(genreList);
-	artists.renderArray(artistList);
-	albums.renderArray(albumList);
-
+		genresList.renderArray(genres);
+		artistsList.renderArray(artists);
+		albumsList.renderArray(albums);
+	});
+	
 	// As items are selected in each of the genre, artist, and album dgrid lists the
 	// associated value will be set on this stateful object so the main grid can
 	// watch for updates and filter accordingly
@@ -109,119 +115,121 @@ function(List, Grid, Selection, Keyboard, Hider, declare, arrayUtil, Stateful, s
 
 	// This function is used further down by the select handler for the artists list.
 	// It builds a filtered list of album names depending on the selected genre and artist.
-	function getFilteredAlbumList(gridFilter, songStore, selectedArtist){
+	function getFilteredAlbumList(gridFilter, songStore, selectedArtist) {
 		var filterOptions = {};
 		var filteredObjects;
 		var filteredAlbumList;
 
-		if(gridFilter.get('Genre')){
-			filterOptions.Genre = gridFilter.get('Genre');
+		if (gridFilter.get('genre')) {
+			filterOptions.genre = gridFilter.get('genre');
 		}
 
-		if(selectedArtist){
-			filterOptions.Artist = selectedArtist;
+		if (selectedArtist) {
+			filterOptions.artist = selectedArtist;
 		}
 		filteredObjects = songStore.filter(filterOptions).fetch();
-		filteredAlbumList = unique(arrayUtil.map(filteredObjects, pickField('Album')));
-		filteredAlbumList.unshift("All (" + filteredAlbumList.length + " Album" + (filteredAlbumList.length !== 1 ? "s" : "") + ")");
+		filteredAlbumList = unique(arrayUtil.map(filteredObjects, pickField('album')));
+		filteredAlbumList.unshift('All (' + filteredAlbumList.length +
+			' Album' + (filteredAlbumList.length !== 1 ? 's' : '') + ')');
 
 		return filteredAlbumList;
 	}
 
-	gridFilter.watch(function(){
+	gridFilter.watch(function () {
 		var filter;
 
-		if(this.Genre || this.Artist || this.Album){
+		if (this.genre || this.artist || this.album) {
 			filter = {};
 
-			if(this.Genre){
-				filter.Genre = this.Genre;
+			if (this.genre) {
+				filter.genre = this.genre;
 			}
-			if(this.Artist){
-				filter.Artist = this.Artist;
+			if (this.artist) {
+				filter.artist = this.artist;
 			}
-			if(this.Album){
-				filter.Album = this.Album;
+			if (this.album) {
+				filter.album = this.album;
 			}
 
 			grid.set('collection', songStore.filter(filter));
 		}
-		else{
-			if(grid.collection !== songStore){
+		else {
+			if (grid.collection !== songStore) {
 				grid.set('collection', songStore);
 			}
 		}
 	});
 
 	// start listening for selections on the lists.
-	genres.on("dgrid-select", function(event){
+	genresList.on('dgrid-select', function (event) {
 		// filter the albums, artists and grid
 		var row = event.rows[0];
 		var selectedGenre = row.data;
 		var filteredObjects;
 		var filteredArtistList;
 
-		if(row.id == "0"){
+		if (row.id === '0') {
 			// remove filtering
-			gridFilter.set('Genre', undefined);
-			filteredArtistList = artistList;
+			gridFilter.set('genre', undefined);
+			filteredArtistList = artists;
 		}
 		else {
-			gridFilter.set('Genre', selectedGenre);
+			gridFilter.set('genre', selectedGenre);
 			// filter the store on the current genre
-			filteredObjects = songStore.filter({ Genre: selectedGenre }).fetch();
+			filteredObjects = songStore.filter({ genre: selectedGenre }).fetch();
 			// map the full album objects to a unique array of artist names (strings)
-			filteredArtistList = unique(arrayUtil.map(filteredObjects, pickField('Artist')));
+			filteredArtistList = unique(arrayUtil.map(filteredObjects, pickField('artist')));
 			// add the "All" option at the top
-			filteredArtistList.unshift("All (" + filteredArtistList.length + " Artist" + (filteredArtistList.length !== 1 ? "s" : "") + ")");
+			filteredArtistList.unshift('All (' + filteredArtistList.length +
+				' Artist' + (filteredArtistList.length !== 1 ? 's' : '') + ')');
 		}
 		
-		artists.refresh();	// clear contents
-		artists.renderArray(filteredArtistList);
-		artists.select(0); // reselect "all", triggering albums+grid refresh
+		artistsList.refresh();	// clear contents
+		artistsList.renderArray(filteredArtistList);
+		artistsList.select('0'); // reselect "all", triggering albums+grid refresh
 	});
 
-	artists.on("dgrid-select", function(event){
+	artistsList.on('dgrid-select', function (event) {
 		// filter the albums, grid
 		var row = event.rows[0];
 		var selectedArtist = row.data;
 		var filteredAlbumList;
 
-		if(row.id == "0"){
-			gridFilter.set('Artist', undefined);
+		if (row.id === '0') {
+			gridFilter.set('artist', undefined);
 
-			if(gridFilter.get('Genre')){
+			if (gridFilter.get('genre')) {
 				// filter only by genre
 				filteredAlbumList = getFilteredAlbumList(gridFilter, songStore);
 			} else {
 				// remove filtering entirely
-				filteredAlbumList = albumList;
+				filteredAlbumList = albums;
 			}
 		}
 		else {
 			// create filter based on artist
-			gridFilter.set('Artist', selectedArtist);
+			gridFilter.set('artist', selectedArtist);
 			filteredAlbumList = getFilteredAlbumList(gridFilter, songStore, selectedArtist);
 		}
 
-		albums.refresh(); // clear contents
-		albums.renderArray(filteredAlbumList);
-		albums.select(0); // reselect "all" item, triggering grid refresh
+		albumsList.refresh(); // clear contents
+		albumsList.renderArray(filteredAlbumList);
+		albumsList.select('0'); // reselect "all" item, triggering grid refresh
 	});
 
-	albums.on("dgrid-select", function(event){
+	albumsList.on('dgrid-select', function (event) {
 		// filter the grid
 		var row = event.rows[0];
 		var selectedAlbum = row.data;
 
-		if(row.id == "0"){
+		if (row.id === '0') {
 			// show all albums
-			gridFilter.set('Album', undefined);
+			gridFilter.set('album', undefined);
 		} else {
-			gridFilter.set('Album', selectedAlbum);
+			gridFilter.set('album', selectedAlbum);
 		}
 	});
 
 	// set the initial selections on the lists.
-	genres.select(0);
+	genresList.select('0');
 });
--- demos/todo/todo.js
+++ demos/todo/todo.js
@@ -1,25 +1,26 @@
 define([
-	"dgrid/OnDemandGrid",
-	"dgrid/Selection",
-	"dgrid/Editor",
-	"dgrid/extensions/DnD",
-	"dojo/_base/declare",
-	"dojo/json",
-	"dojo/on",
-	"dstore/Memory",
-	"dstore/Observable",
-	"put-selector/put",
-	"dojo/domReady!"
-], function(OnDemandGrid, Selection, Editor, DnD, declare, JSON, on, Memory, Observable, put){
+	'dgrid/OnDemandGrid',
+	'dgrid/Selection',
+	'dgrid/Editor',
+	'dgrid/extensions/DnD',
+	'dojo/_base/declare',
+	'dojo/json',
+	'dojo/on',
+	'dstore/Memory',
+	'dstore/Observable',
+	'put-selector/put',
+	'dojo/domReady!'
+], function (OnDemandGrid, Selection, Editor, DnD, declare, JSON, on, Memory, Observable, put) {
 	// Create DOM
-	var container = put("div#container");
-	var itemForm = put(container, "form#itemForm.actionArea.topArea");
-	var taskField = put(itemForm, "input#txtTask[name=task]");
-	var addButton = put(itemForm, "button[type=submit]", "Add");
-	var listNode = put(container, "div#list");
-	var removeArea = put(container, "div.actionArea.bottomArea");
-	var removeSelectedButton = put(removeArea, "button[type=button]", "Remove Selected");
-	var removeCompletedButton = put(removeArea, "button[type=button]", "Remove Completed");
+	var container = put('div#container');
+	var itemForm = put(container, 'form#itemForm.actionArea.topArea');
+	var taskField = put(itemForm, 'input#txtTask[name=task]');
+	put(itemForm, 'button[type=submit]', 'Add');
+	
+	var listNode = put(container, 'div#list');
+	var removeArea = put(container, 'div.actionArea.bottomArea');
+	var removeSelectedButton = put(removeArea, 'button[type=button]', 'Remove Selected');
+	var removeCompletedButton = put(removeArea, 'button[type=button]', 'Remove Completed');
 
 	put(document.body, container);
 	
@@ -28,9 +29,9 @@ define([
 	if (window.localStorage) {
 		// add functionality for saving/recalling from localStorage
 		storeMixins.push(declare(null, {
-			STORAGE_KEY: "dgrid_demo_todo_list",
+			STORAGE_KEY: 'dgrid_demo_todo_list',
 
-			constructor: function(){
+			constructor: function () {
 				var self = this;
 				var jsondata = localStorage[this.STORAGE_KEY];
 
@@ -46,33 +47,31 @@ define([
 	var Store = declare(storeMixins);
 
 	var store = new Store({
-		idProperty: "summary",
-		beforeId: "order"
+		idProperty: 'summary'
 	});
 
 	var grid = new (declare([OnDemandGrid, Selection, DnD, Editor]))({
-		sort: "order",
 		collection: store,
 		columns: {
 			completed: {
-				editor: "checkbox",
-				label: " ",
+				editor: 'checkbox',
+				label: ' ',
 				autoSave: true,
 				sortable: false
 			},
 			summary: {
-				field: "_item", // get whole item for use by formatter
-				label: "TODOs",
+				field: '_item', // get whole item for use by formatter
+				label: 'TODOs',
 				sortable: false,
-				formatter: function(item){
-					return "<div" + (item.completed ? ' class="completed"' : "") +
-						">" + item.summary + "</div>";
+				formatter: function (item) {
+					return '<div' + (item.completed ? ' class="completed"' : '') +
+						'>' + item.summary + '</div>';
 				}
 			}
 		}
 	}, listNode);
 	
-	on(itemForm, "submit", function(event){
+	on(itemForm, 'submit', function (event) {
 		event.preventDefault();
 
 		// allow overwrite if already exists (by using put, not add)
@@ -80,11 +79,10 @@ define([
 			completed: false,
 			summary: taskField.value
 		});
-		taskField.value = "";
-		return false;
+		taskField.value = '';
 	});
 
-	on(removeSelectedButton, "click", function(){
+	on(removeSelectedButton, 'click', function () {
 		for (var i in grid.selection) {
 			// Each key in the selection map is the id of the item,
 			// so we can pass it directly to store.remove.
@@ -92,19 +90,19 @@ define([
 		}
 	});
 
-	on(removeCompletedButton, "click", function(){
+	on(removeCompletedButton, 'click', function () {
 		// query for all completed items and remove them
 		store.filter({ completed: true }).fetch().forEach(function (item) {
 			store.remove(item[store.idProperty]);
 		});
 	});
 	
-	if(window.localStorage){
+	if (window.localStorage) {
 		// add extra button to clear the localStorage key we're using
-		var button = put(removeArea, "button[type=button]",
-			"Clear localStorage");
+		var button = put(removeArea, 'button[type=button]',
+			'Clear localStorage');
 
-		on(button, "click", function(){
+		on(button, 'click', function () {
 			localStorage.removeItem(store.STORAGE_KEY);
 			// remove all items in grid the quick way (no need to iteratively remove)
 			store.setData([]);
