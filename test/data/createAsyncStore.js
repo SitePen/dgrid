dd048dd6543dcf589ce27c7d3fae790b4f217ba6
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2014-08-15 18:22:44 -0400

    Fix createAsyncStore so sub-collections are also async
    This code is based on DeferredWrapper's, but uses declare to extend
    instead of object delegation so that the prototype is carried over to
    sub-collections.
    
    Note that DeferredWrapper stores currently have the same problem;
    we should likely change them to use createAsyncStore as well.

--- test/data/createAsyncStore.js
+++ test/data/createAsyncStore.js
@@ -1,22 +1,58 @@
 define([
-	"dojo/_base/Deferred",
-	"./createSyncStore"
-], function(Deferred, createSyncStore){
-	return function createAsyncStore(kwArgs){
-		var store = createSyncStore(kwArgs),
-			syncFetch = store.fetch;
+	'dojo/_base/declare',
+	'dojo/_base/lang',
+	'dojo/Deferred',
+	'dstore/Memory',
+	'dstore/Observable',
+	'dstore/QueryResults'
+], function (declare, lang, Deferred, Memory, Observable, QueryResults) {
+	// summary:
+	//		Creates a store that wraps the delegate store's query results and total in Deferred
+	//		instances. If delay is set, the Deferreds will be resolved asynchronously after delay +/-50%
+	//		milliseconds to simulate network requests that may come back out of order.
+	var AsyncStore = declare(Memory, {
+		delay: 200,
+		randomizeDelay: false,
 
-		store.fetch = function asyncFetch() {
-			var results = syncFetch.apply(this, arguments),
-				def = new Deferred(function(){
-					clearTimeout(timer);
-				}),
-				timer = setTimeout(function(){
-					def.resolve(results);
-				}, 200);
-			return def.promise;
-		};
+		fetch: function () {
+			var actualData = this.inherited(arguments);
+			var actualTotal = actualData.totalLength;
+			var resultsDeferred = new Deferred();
+			var totalDeferred = new Deferred();
 
-		return store;
-	};
+			function resolveResults() {
+				resultsDeferred.resolve(actualData);
+			}
+			function resolveTotal() {
+				totalDeferred.resolve(actualTotal);
+			}
+
+			setTimeout(resolveTotal, this.delay * (this.randomizeDelay ? Math.random() + 0.5 : 1));
+			setTimeout(resolveResults, this.delay * (this.randomizeDelay ? Math.random() + 0.5 : 1));
+
+			return new QueryResults(resultsDeferred, {
+				totalLength: totalDeferred
+			});
+		},
+
+		fetchRange: function (kwArgs) {
+			// dstore/Memory currently handles the data as potentially async
+			// but not the length.
+			// TODO: Revisit/remove when dstore is always-promise.
+			var results = this.fetch();
+			return new QueryResults(results.then(function (data) {
+				return data.slice(kwArgs.start, kwArgs.end);
+			}), {
+				totalLength: results.then(function (data) {
+					return data.length;
+				})
+			});
+		}
+	});
+
+	var ObservableAsyncStore = declare([ AsyncStore, Observable ]);
+	
+	return function (kwArgs) {
+		return new ObservableAsyncStore(kwArgs);
+	}
 });
