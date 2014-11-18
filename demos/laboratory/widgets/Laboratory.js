bb94dcc860e318325b9725652b9ce0af78d550b5
Ed Hager <ehager@sitepen.com>
2014-11-18 11:50:55 -0600

    Laboratory: Only add initial columns after feature editor forms load

--- demos/laboratory/widgets/Laboratory.js
+++ demos/laboratory/widgets/Laboratory.js
@@ -68,12 +68,15 @@ define([
 			var columnEditor = this.columnEditor;
 			this.inherited(arguments);
 
-			this.featureEditor.startup();
-			columnEditor.startup();
+			this.featureEditor.startup().then(function () {
+				columnEditor.startup();
 
-			// Add a couple of columns by default
-			columnEditor.addColumn('First Name');
-			columnEditor.addColumn('Last Name');
+				// Add a couple of columns by default;
+				// wait until after FeatureEditor's startup promise resolves,
+				// to give forms a chance to react to column addition/removal (e.g. Tree)
+				columnEditor.addColumn('First Name');
+				columnEditor.addColumn('Last Name');
+			});
 		},
 
 		selectTab: function (evt) {
@@ -167,7 +170,7 @@ define([
 				storeModules = [ 'Memory', 'Trackable' ];
 
 				if (treeExpandoColumn) {
-						storeModules.push('TreeStoreMixin');
+					storeModules.push('TreeStoreMixin');
 				}
 
 				gridConfig.dataDeclaration = 'var store = new (declare([' + storeModules.join(', ') + ']))({\n' +
