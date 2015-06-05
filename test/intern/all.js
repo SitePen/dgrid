1767584e861f2d5aeb5f8a36de0973eef6660411
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2015-06-04 22:07:49 -0400

    DijitRegistry: Add placeAt (delegating to _WidgetBase's)
    Includes unit tests.
    
    Resolves #1135.

--- test/intern/all.js
+++ test/intern/all.js
@@ -9,6 +9,7 @@ define([
 	'intern/node_modules/dojo/has!host-browser?./core/trackable',
 	'intern/node_modules/dojo/has!host-browser?./extensions/ColumnHider',
 	'intern/node_modules/dojo/has!host-browser?./extensions/CompoundColumns',
+	'intern/node_modules/dojo/has!host-browser?./extensions/DijitRegistry',
 	'intern/node_modules/dojo/has!host-browser?./extensions/Pagination',
 	'intern/node_modules/dojo/has!host-browser?./mixins/ColumnSet',
 	'intern/node_modules/dojo/has!host-browser?./mixins/Editor',
