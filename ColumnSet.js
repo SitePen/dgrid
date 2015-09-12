1f0be46563745a94bec57060777a67888880f552
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-09-11 17:21:43 -0700

    Fix #1179: ColumnSet: Avoid wheel/touch event errors in empty viewport
    (cherry picked from commit 32b15f5cd07565351683086ed1afd3dae860ec9d)

--- ColumnSet.js
+++ ColumnSet.js
@@ -1,6 +1,7 @@
 define([
 	'dojo/_base/declare',
 	'dojo/_base/lang',
+	'dojo/dom-class',
 	'dojo/on',
 	'dojo/aspect',
 	'dojo/query',
@@ -9,7 +10,7 @@ define([
 	'put-selector/put',
 	'dojo/_base/sniff',
 	'xstyle/css!./css/columnset.css'
-], function (declare, lang, on, aspect, query, has, miscUtil, put) {
+], function (declare, lang, domClass, on, aspect, query, has, miscUtil, put) {
 	has.add('event-mousewheel', function (global, document, element) {
 		return 'onmousewheel' in element;
 	});
@@ -52,6 +53,13 @@ define([
 		return subset;
 	}
 
+	function isRootNode(node, rootNode) {
+		// If we've reached the top-level node for the grid then there is no parent column set.
+		// This guard prevents an error when scroll is initated over some node in the grid that is not a descendant of
+		// a column set. This can happen in a grid that has empty space below its rows (grid is taller than the rows).
+		return (rootNode && node === rootNode) || domClass.contains(node, 'dgrid');
+	}
+
 	function findParentColumnSet(node, root) {
 		// WebKit will invoke mousewheel handlers with an event target of a text
 		// node; check target and if it's not an element node, start one node higher
@@ -59,9 +67,14 @@ define([
 		if (node.nodeType !== 1) {
 			node = node.parentNode;
 		}
+
 		while (node && !query.matches(node, '.dgrid-column-set[' + colsetidAttr + ']', root)) {
+			if (isRootNode(node, root)) {
+				return null;
+			}
 			node = node.parentNode;
 		}
+
 		return node;
 	}
 
