c95a436808e3567df643eff6f75dfd1b7500e3c8
Bradley Maier <bmaier@sitepen.com>
2015-12-21 13:02:44 -0500

    Preload all skins for skin test Fixes #1204

--- test/skinUtil.js
+++ test/skinUtil.js
@@ -7,20 +7,14 @@ define([
 	// Honor skin and RTL specification from query param
 	var skin = params.skin || 'claro';
 	var rtl = params.dir === 'rtl';
-	var skins = ['claro', 'tundra', 'nihilo', 'soria', 'slate', 'sage', 'cactus'];
 
 	// Add skin class to body
 	document.body.className = skin;
 	// Add dir=rtl to body if specified in query
 	rtl && (document.body.dir = 'rtl');
 
-	var link = document.createElement('link');
-	link.rel = 'stylesheet';
-	link.href = require.toUrl('dgrid/css/skins/' + skin + '.css');
-	document.getElementsByTagName('head')[0].appendChild(link);
-
 	return {
-		renderLinks: function () {
+		renderLinks: function (skins) {
 			return arrayUtil.map(skins, function (s) {
 				return s === skin ? s :
 					'<a href="skin.html?skin=' + s + (rtl ? '&dir=rtl' : '') + '">' + s + '</a>';
