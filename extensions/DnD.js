2c437029564c54d1d1e7c6bc722a0de2d0856766
Ed Hager <ehager@sitepen.com>
2016-01-08 16:06:12 -0800

    Fix #1241 - set DnD property allowNested if Tree mixin is used.
    Add code to dgrid/extensions/DnD to look for us of the dgrid/Tree mixin.
    If Tree is being used, set `allowNested` to true which allows dojo/dnd to
    properly identify the child row container as a valid drop target.
    
    Also update the DnD_Tree test page to allow testing this.
    
    (cherry picked from commit 0a108c3266731612ffa3b9d50594466d60335775)

--- extensions/DnD.js
+++ extensions/DnD.js
@@ -250,14 +250,18 @@ define([
 
 			// Make the grid's content a DnD source/target.
 			var Source = this.dndConstructor || GridDnDSource;
-			this.dndSource = new Source(
-				this.bodyNode,
-				lang.mixin(this.dndParams, {
-					// add cross-reference to grid for potential use in inter-grid drop logic
-					grid: this,
-					dropParent: this.contentNode
-				})
-			);
+
+			var dndParams = lang.mixin(this.dndParams, {
+				// add cross-reference to grid for potential use in inter-grid drop logic
+				grid: this,
+				dropParent: this.contentNode
+			});
+			if (typeof this.expand === 'function') {
+				// If the Tree mixin is being used, allowNested needs to be set to true for DnD to work properly
+				// with the child rows.  Without it, child rows will always move to the last child position.
+				dndParams.allowNested = true;
+			}
+			this.dndSource = new Source(this.bodyNode, dndParams);
 
 			// Set up select/deselect handlers to maintain references, in case selected
 			// rows are scrolled out of view and unrendered, but then dragged.
