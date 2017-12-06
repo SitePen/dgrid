42be06947e4b9eb6789d18022d645038e5781c42
Dmitriy Orlov <deo@seventest.ru>
2017-12-06 13:20:19 +0300

    Add Russian language support

---
+++ extensions/nls/ru/pagination.js
@@ -0,0 +1,13 @@
+define({
+	status: '${start} - ${end} записей из ${total}',
+	gotoFirst: 'Перейти к первой странице',
+	gotoNext: 'Перейти к следующей странице',
+	gotoPrev: 'Перейти к предыдущей странице',
+	gotoLast: 'Перейти к последней странице',
+	gotoPage: 'Перейти к странице',
+	jumpPage: 'К странице',
+	rowsPerPage: 'Количество строк на странице',
+	notCompatibleWithOnDemand: 'Расширение Pagination несовместимо с dgrid/OnDemandList ' +
+                        'и dgrid/OnDemandGrid. Используйте, пожалуйста, dgrid/List или dgrid/Grid в качестве базового класса.'
+
+});
